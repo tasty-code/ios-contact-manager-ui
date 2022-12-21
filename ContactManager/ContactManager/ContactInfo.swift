e2d76a98f81d34f9aa42b081f8f1a1d2c24585be
bomyuniverse <bomi@soongsil.ac.kr>
2022-12-21 15:59:04 +0900

    Chore: contactInfo 타입을 파일로 분리
    네비게이터: Jason, 드라이버: Blue
    - 가독성을 위해 파일 분리

---
+++ ContactManager/ContactManager/ContactInfo.swift
@@ -0,0 +1,14 @@
+//
+//  ContactInfo.swift
+//  ContactManager
+//
+//  Created by 김보미 on 2022/12/21.
+//
+
+import Foundation
+
+struct ContactInfo {
+    let name: String
+    let age: String
+    let phoneNumber: String
+}
---
+++ ContactManager/ContactManager/PrintMessage.swift
@@ -0,0 +1,8 @@
+//
+//  PrintMessage.swift
+//  ContactManager
+//
+//  Created by 김보미 on 2022/12/21.
+//
+
+import Foundation
