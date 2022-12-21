95855ac5ed4c0af85264aa615e27b93db939d091
bomyuniverse <bomi@soongsil.ac.kr>
2022-12-21 16:02:18 +0900

    Feat: PrintMessage 타입을 통해 출력되는 메세지 구현
    - CustomStringConvertible 프로토콜을 채택하여 출력 케이스를 호출하여 사용

--- ContactManager/ContactManager/PrintMessage.swift
+++ ContactManager/ContactManager/PrintMessage.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+enum PrintMessage: String, CustomStringConvertible {
+    case startComment = "연락처 정보를 입력해주세요 : "
+    case correctContactInfo = "입력한 정보는 "
+    case invalidAge = "입력한 나이 정보가 잘못되었습니다. 입력 형식을 확인해주세요."
+    case invalidPhoneNumber = "입력한 연락처 정보가 잘못되었습니다. 입력 형식을 확인해주세요."
+    case nothingUserInput = "아무것도 입력되지 않았습니다. 입력 형식을 확인해주세요."
+    
+    var description: String {
+        return "\(self.rawValue)"
+    }
+}
