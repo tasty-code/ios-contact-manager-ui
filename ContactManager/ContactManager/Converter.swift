74d1bab98ea514d5e5c119278a1b45d847cfb27d
bomyuniverse <bomi@soongsil.ac.kr>
2022-12-22 14:11:50 +0900

    Feat: Converter 클래스 생성 및 문자열 변환 메서드 추가

---
+++ ContactManager/ContactManager/Converter.swift
@@ -0,0 +1,14 @@
+//
+//  Converter.swift
+//  ContactManager
+//
+//  Created by 김보미 on 2022/12/22.
+//
+
+import Foundation
+
+class Converter {
+    func convertToString(_ word: [Character]) -> String {
+        return String(word)
+    }
+}
