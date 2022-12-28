d696869e227949fb6d4576a2d27880f507ecf034
Jason <ggj00125@gmail.com>
2022-12-28 11:06:08 +0900

    Feat: 요구사항에서 빠져있었던 출력문 추가
    - invalidName 케이스는 임의로 작성하여 오류 위치를 좀 더 용이하게 파악하기 위해 작성
    - validUserInput 타입메서드는 유저가 입력한 값에 문제가 없는 경우에 출력하기 위해 작성

--- ContactManager/ContactManager/Storage/PrintMessage.swift
+++ ContactManager/ContactManager/Storage/PrintMessage.swift
@@ -10,9 +10,19 @@ import Foundation
 enum PrintMessage: String, CustomStringConvertible {
     case startComment = "연락처 정보를 입력해주세요 : "
     case correctContactInfo = "입력한 정보는 "
+    case invalidName = "입력한 이름 정보가 잘못되었습니다. 입력 형식을 확인해주세요."
     case invalidAge = "입력한 나이 정보가 잘못되었습니다. 입력 형식을 확인해주세요."
     case invalidPhoneNumber = "입력한 연락처 정보가 잘못되었습니다. 입력 형식을 확인해주세요."
     case nothingUserInput = "아무것도 입력되지 않았습니다. 입력 형식을 확인해주세요."
+    
+    static func validUserInput(value: [String]) -> Bool {
+        let name = value[0]
+        let age = value[1]
+        let phoneNumber = value[2]
+        print(PrintMessage.correctContactInfo, terminator: "")
+        print("\(name)세 \(age)(\(phoneNumber))입니다. \n")
+        return true
+    }
     
     var description: String {
         return self.rawValue
