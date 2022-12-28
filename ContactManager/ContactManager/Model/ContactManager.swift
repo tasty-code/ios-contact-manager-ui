0c598433d024389f55624fa6f2a2fc1663af9cc1
Jason <ggj00125@gmail.com>
2022-12-28 11:10:45 +0900

    Refactor: 기존 코드 수정
    - identifier 프로퍼티를 Bool타입으로 변경하여 좀 더 동작여부를 명확하게 하기 위해 수정
    - 아무런 입력이 없을 때 다시 입력을 받을 수 있도록 조건문 추가
    - 기존 검증 방법의 변화로 인해 불필요해진 코드 삭제

--- ContactManager/ContactManager/Model/ContactManager.swift
+++ ContactManager/ContactManager/Model/ContactManager.swift
@@ -8,39 +8,32 @@
 import Foundation
 
 final class ContactManager {
-    var contactInformation: [ContactInformation] = []
+    var contactInformation = [ContactInformation]()
     let detector = Detector()
     let convertor = Converter()
     let checker = Checker()
     
     func startProcess() {
-        var identifier = ""
+        var identifier: Bool
         
         repeat {
             print(PrintMessage.startComment)
             let receiveUserInputValues = userInputValue()
+            if receiveUserInputValues == "" {
+                print(PrintMessage.nothingUserInput)
+                break
+            }
             let convertedUserInputValues = convertor.convertToCharacter(this: receiveUserInputValues)
-            
             let removedBlankUserInputValues = detector.excludeSpaceWord(convertedUserInputValues)
-            let slashIndexArray = detector.extractIndexWithSlash(from: removedBlankUserInputValues)
+            let combinedUserInputValues = convertor.convertToString(removedBlankUserInputValues)
+            let splitedUserInputValues = combinedUserInputValues.split(separator: "/").map{ String($0) }
             
-            guard let firstSlashIndex = slashIndexArray.first else {
-                return
-            }
-            guard let lastSlashIndex = slashIndexArray.last else {
-                return
-            }
             
-            let inputValuesCount = convertedUserInputValues.count
-            
-            let nameWord = joinWords(first: 0, second: firstSlashIndex, this: removedBlankUserInputValues)
-            let ageWord = joinWords(first: firstSlashIndex, second: lastSlashIndex, this: removedBlankUserInputValues)
-            let phoneNumberWord = joinWords(first: lastSlashIndex, second: inputValuesCount, this: removedBlankUserInputValues)
-            
-            let nameString = checker.checkCorrectWord(target: convertor.convertToString(nameWord))
-            let ageString = checker.checkCorrectWord(target: convertor.convertToString(ageWord))
-            let phoneNumberString = checker.checkCorrectWord(target: convertor.convertToString(phoneNumberWord))
-        } while identifier == ""
+            let checkUserInputValues = checker.checkCorrectWord(target: splitedUserInputValues)
+            let receivecValues = checker.checkCorrectWord(target: checkUserInputValues)
+
+            identifier = PrintMessage.validUserInput(value: receivecValues)
+        } while identifier == true
     }
 }
 
@@ -50,19 +43,5 @@ extension ContactManager: InputPossible {
             return "F"
         }
         return userInput
-    }
-}
-
-extension ContactManager {
-    func joinWords(first index1: Int, second index2: Int, this word: [Character]) -> [Character] {
-        var usefulValue = [Character]()
-        for index in index1..<index2 {
-            let tempStorage = word[index]
-            if tempStorage == "/" {
-                continue
-            }
-            usefulValue.append(tempStorage)
-        }
-        return usefulValue
     }
 }
