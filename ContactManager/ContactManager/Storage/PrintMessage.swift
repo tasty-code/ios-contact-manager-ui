465244e76ab8c86d5d9673293b0ea90d879419cf
Jason <ggj00125@gmail.com>
2022-12-28 11:25:28 +0900

    Refactor: 기존 출력문에 개행 추가 및 타입메서드에 조건 추가

--- ContactManager/ContactManager/Storage/PrintMessage.swift
+++ ContactManager/ContactManager/Storage/PrintMessage.swift
@@ -10,17 +10,23 @@ import Foundation
 enum PrintMessage: String, CustomStringConvertible {
     case startComment = "연락처 정보를 입력해주세요 : "
     case correctContactInfo = "입력한 정보는 "
-    case invalidName = "입력한 이름 정보가 잘못되었습니다. 입력 형식을 확인해주세요."
-    case invalidAge = "입력한 나이 정보가 잘못되었습니다. 입력 형식을 확인해주세요."
-    case invalidPhoneNumber = "입력한 연락처 정보가 잘못되었습니다. 입력 형식을 확인해주세요."
-    case nothingUserInput = "아무것도 입력되지 않았습니다. 입력 형식을 확인해주세요."
+    case invalidName = "입력한 이름 정보가 잘못되었습니다. 입력 형식을 확인해주세요. \n"
+    case invalidAge = "입력한 나이 정보가 잘못되었습니다. 입력 형식을 확인해주세요. \n"
+    case invalidPhoneNumber = "입력한 연락처 정보가 잘못되었습니다. 입력 형식을 확인해주세요. \n"
+    case invalidFormat = "입력한 정보의 형식이 맞지않습니다. 올바른 형식을 확인해주세요. \n"
+    case nothingUserInput = "아무것도 입력되지 않았습니다. 입력 형식을 확인해주세요. \n"
     
     static func validUserInput(value: [String]) -> Bool {
-        let name = value[0]
-        let age = value[1]
-        let phoneNumber = value[2]
-        print(PrintMessage.correctContactInfo, terminator: "")
-        print("\(name)세 \(age)(\(phoneNumber))입니다. \n")
+        if value.count == 3 {
+            let name = value[0]
+            let age = value[1]
+            let phoneNumber = value[2]
+            
+            print(PrintMessage.correctContactInfo, terminator: "")
+            print("\(age)세 \(name)(\(phoneNumber))입니다. \n")
+        } else {
+            print(PrintMessage.invalidFormat)
+        }
         return true
     }
     
