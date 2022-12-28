9d86ead522814551dd73f1379eda273cbd8c5524
Jason <ggj00125@gmail.com>
2022-12-28 11:03:12 +0900

    Refactor: 정규표현식 수정
    - 시작(^)과 끝($), 위 표현식만 포함한다(*) 키워드 추가

--- ContactManager/ContactManager/Storage/RegularExpression.swift
+++ ContactManager/ContactManager/Storage/RegularExpression.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 enum RegularExpression: String, CustomStringConvertible {
-    case namePattern = "[A-Za-z]"
-    case agePattern = "[1-999]"
-    case phoneNumberPattern = "^0([0-9]{1,2})-([0-9]{3,4})-([0-9]{4})"
+    case namePattern = "^[A-Za-z]*$"
+    case agePattern = "^[0-999]*$"
+    case phoneNumberPattern = "^0([0-9]{1,2})-([0-9]{3,4})-([0-9]{4})*$"
     
     var description: String {
         return self.rawValue
