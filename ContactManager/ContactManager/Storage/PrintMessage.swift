efb5b7d83e86a2cab832fe6809a6cc81cdc61378
Jason <ggj00125@gmail.com>
2022-12-29 10:40:17 +0900

    Feat: 연락처 검색 기능 searchContact(list:) 메서드로 기능 구현

--- ContactManager/ContactManager/Storage/PrintMessage.swift
+++ ContactManager/ContactManager/Storage/PrintMessage.swift
@@ -11,6 +11,7 @@ enum PrintMessage: String, CustomStringConvertible {
     case startComment = "1) 연락처 추가 2) 연락처 목록보기 3) 연락처 검색 x) 종료 \n메뉴를 선택해주세요 : "
     case requestContactInfo = "연락처 정보를 입력해주세요 : "
     case correctContactInfo = "입력한 정보는 "
+    case searchContactFromName = "연락처에서 찾을 이름을 입력해주세요: "
     case invalidName = "입력한 이름 정보가 잘못되었습니다. 입력 형식을 확인해주세요. \n"
     case invalidAge = "입력한 나이 정보가 잘못되었습니다. 입력 형식을 확인해주세요. \n"
     case invalidPhoneNumber = "입력한 연락처 정보가 잘못되었습니다. 입력 형식을 확인해주세요. \n"
@@ -32,6 +33,13 @@ enum PrintMessage: String, CustomStringConvertible {
     
     static func viewContact(list: [ContactInformation]) {
         list.forEach { element in
+            print("- \(element.name) / \(element.age) / \(element.phoneNumber)")
+        }
+    }
+    
+    static func searchContact(list: [ContactInformation], word: String) {
+        let confirmContactArray = list.filter{$0.name == word}
+        confirmContactArray.forEach { element in
             print("- \(element.name) / \(element.age) / \(element.phoneNumber)")
         }
     }
