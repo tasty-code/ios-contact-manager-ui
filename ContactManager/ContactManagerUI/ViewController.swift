37e3f125bebee21abf4ff6ed93f9fae273642013
sloDevz <onec555@gmail.com>
2023-01-31 14:52:36 +0900

    feat: UITableView에 Cell 추가 및 기본적인 TableView출력 구현

--- ContactManager/ContactManagerUI/ViewController.swift
+++ ContactManager/ContactManagerUI/ViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    let dummyPerson: Person = Person(name: "알리", age: 10, phoneNum: "010-000-0000")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+                
         tableView.delegate = self
         tableView.dataSource = self
     }
 }
-
 
 extension ViewController: UITableViewDelegate {
     
@@ -26,12 +26,17 @@ extension ViewController: UITableViewDelegate {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Int()
+        return 20
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        let cellIdentifier = "ContactListCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+
+        cell.textLabel?.text = dummyPerson.name + "(\(dummyPerson.age))"
+        cell.detailTextLabel?.text = dummyPerson.phoneNum
+
+        return cell
     }
-    
-    
 }
