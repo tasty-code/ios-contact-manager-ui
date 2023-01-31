b7bd63405a1babee8f0ef69b54de3e49dec8c7c6
bomyuniverse <71758542+calledBlu@users.noreply.github.com>
2023-01-31 11:25:27 +0900

    Feat: TableView 구현 및 스토리보드와 연결

---
+++ ContactManager/ContactManagerUI/Controller/ContactManagerTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ContactManagerTableViewController.swift
+//  ContactManagerUI
+//
+//  Created by Blu on 2023/01/31.
+//
+
+import UIKit
+
+class ContactManagerTableViewController: UITableViewController {
+
+    @IBOutlet private weak var contactManagerTableView: UITableView!
+    private var contactDataSource = ContactDataSource()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureTableView()
+    }
+
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self.contactDataSource
+    }
+}
+
+class ContactDataSource: NSObject, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
