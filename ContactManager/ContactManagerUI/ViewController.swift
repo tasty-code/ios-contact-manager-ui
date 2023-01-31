742293c7903775f1dc99071506491a92db9e40e2
zhunhe <xuzhunhe@gmail.com>
2023-01-31 11:20:53 +0900

    chore: Create ContactManagerUI target

---
+++ ContactManager/ContactManagerUI/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ContactManagerUI
+//
+//  Created by jun on 2023/01/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
