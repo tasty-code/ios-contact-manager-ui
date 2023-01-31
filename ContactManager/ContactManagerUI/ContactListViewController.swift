a276eaebce30ae275859edb3ec975e074a4cbe31
devxsby <devxsby@gmail.com>
2023-01-31 16:35:27 +0900

    chore: 파일명 및 변수명 수정

--- ContactManager/ContactManagerUI/ContactListViewController.swift
+++ ContactManager/ContactManagerUI/ContactListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ContactListViewController.swift
 //  ContactManagerUI
 //
 //  Created by DONGWOOK SEO on 2023/01/30.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class ContactListViewController: UIViewController {
     
     private let dummyContactList = Person.dummyData
 
-    @IBOutlet weak private var tableView: UITableView!
+    @IBOutlet weak var contactListTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,24 +19,23 @@ final class ViewController: UIViewController {
     }
     
     private func setDelegate() {
-        tableView.delegate = self
-        tableView.dataSource = self
+        contactListTableView.delegate = self
+        contactListTableView.dataSource = self
     }
 }
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
+extension ContactListViewController: UITableViewDelegate, UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dummyContactList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cellIdentifier = "ContactTableViewCell"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
         
         cell.textLabel?.text = dummyContactList[indexPath.row].name + "(\(dummyContactList[indexPath.row].age))"
         cell.detailTextLabel?.text = dummyContactList[indexPath.row].phoneNum
-        
         return cell
     }
 }
