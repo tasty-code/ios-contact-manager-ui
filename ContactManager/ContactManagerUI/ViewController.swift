6e691e8b26adbe85edfd522d8ef4ea0b905ef102
bomyuniverse <71758542+calledBlu@users.noreply.github.com>
2023-01-31 10:36:13 +0900

    Build: iOS Target 추가

---
+++ ContactManager/ContactManagerUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactManagerUI
+//
+//  Created by Blu on 2023/01/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
