79f630f488f13ebbc8d64140df32eded95595dc4
longlivedrgn <kimye9808@naver.com>
2023-01-31 15:33:39 +0900

    feat: table view 생성 및 Cell 구현
    - delegate, datasource 연결

--- contactManager/ContactManagerUI/ViewController.swift
+++ contactManager/ContactManagerUI/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var contactsTableView: UITableView!
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           contactsTableView.delegate = self
+           contactsTableView.dataSource = self
+       }
+   }
+
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIndentifier = "ContactCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIndentifier, for: indexPath)
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
 }
+
+extension ViewController: UITableViewDelegate {
+    
+}
+
 
