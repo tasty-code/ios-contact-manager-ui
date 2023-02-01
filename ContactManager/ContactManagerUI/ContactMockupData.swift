645bbfec121550abc50796dec9cb56d73a9f202c
qwerty3345 <qwerty3345@naver.com>
2023-02-01 10:31:41 +0900

    chore: 연락처 초기 목업 데이터 생성

---
+++ ContactManager/ContactManagerUI/ContactMockupData.swift
@@ -0,0 +1,28 @@
+//
+//  ContactMockupData.swift
+//  ContactManagerUI
+//
+//  Created by Mason Kim on 2023/02/01.
+//
+
+import Foundation
+
+let contacts = [
+    Contact(name: "james", age: 30, phoneNumber: "05-343-2234"),
+    Contact(name: "steven", age: 100, phoneNumber: "047-221-3432"),
+    Contact(name: "hajin", age: 33, phoneNumber: "010-3332-0093"),
+    Contact(name: "mini", age: 99, phoneNumber: "0323-33-2232"),
+    Contact(name: "hojun", age: 45, phoneNumber: "3-444-19343"),
+    Contact(name: "dooly", age: 95, phoneNumber: "932-39443-23"),
+    Contact(name: "lucy", age: 1, phoneNumber: "000-232-03333"),
+    Contact(name: "yo", age: 30, phoneNumber: "323-43-3333"),
+    Contact(name: "joy", age: 65, phoneNumber: "6894-3-3434"),
+    Contact(name: "summer", age: 84, phoneNumber: "9-32-343323"),
+    Contact(name: "green", age: 54, phoneNumber: "895-3432-344"),
+    Contact(name: "jisu", age: 30, phoneNumber: "05-343-2234"),
+    Contact(name: "hana", age: 100, phoneNumber: "047-221-3432"),
+    Contact(name: "mike", age: 33, phoneNumber: "010-3332-0093"),
+    Contact(name: "coco", age: 99, phoneNumber: "0323-33-2232"),
+    Contact(name: "tiger", age: 45, phoneNumber: "3-444-19343"),
+    Contact(name: "molly", age: 95, phoneNumber: "932-39443-23")
+]
