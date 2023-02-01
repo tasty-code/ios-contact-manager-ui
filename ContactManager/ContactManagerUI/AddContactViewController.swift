8dbf36d91076af6f565ad53946f53a72f1bb22d6
sloDevz <onec555@gmail.com>
2023-02-01 23:55:46 +0900

    feat: AddContactViewController와 AddContact 스토리보드 추가, ContactListViewController에 NavigationController 추가 및 add 버튼추가

---
+++ ContactManager/ContactManagerUI/AddContactViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddContactViewController.swift
+//  ContactManagerUI
+//
+//  Created by DONGWOOK SEO on 2023/02/01.
+//
+
+import UIKit
+
+final class AddContactViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
