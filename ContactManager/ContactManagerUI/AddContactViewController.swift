13fb209a0e2bcd52a63e0a569d1c6a16e3459fd7
sloDevz <onec555@gmail.com>
2023-02-06 07:56:46 +0900

    feat: AddContactViewController Cancel,Save 버튼메서드 및 TextField변수 생성

--- ContactManager/ContactManagerUI/AddContactViewController.swift
+++ ContactManager/ContactManagerUI/AddContactViewController.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 final class AddContactViewController: UIViewController {
+    
+    
+    @IBOutlet weak private var nameTextField: UITextField!
+    @IBOutlet weak private var ageTextField: UITextField!
+    @IBOutlet weak private var phoneNumberTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    
+    @IBAction private func tappedCancelButton(_ sender: UIBarButtonItem) {
+    }
+    
+    
+    @IBAction private func tappedSaveButton(_ sender: UIBarButtonItem) {
     }
 }
