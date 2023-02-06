7f15c1c12c38cfbdb96a1084e76e91482bc03a67
Lust3r <s1skyblue1s@gmail.com>
2023-02-06 10:30:29 +0900

    refactor: 클로저를 통해 NavigationBar를 추가하는 코드로 변경 및 레이아웃 설정

--- contactManager/ContactManagerUI/AddContactViewController.swift
+++ contactManager/ContactManagerUI/AddContactViewController.swift
@@ -9,23 +9,38 @@ import UIKit
 
 class AddContactViewController: UIViewController {
 
+    var newContactNavigationBar: UINavigationBar = {
+        let navigationBar = UINavigationBar()
+        let navigationItem = UINavigationItem(title: "새 연락처")
+        let cancelItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: nil, action: nil)
+        let saveItem = UIBarButtonItem(barButtonSystemItem: .save, target: nil, action: nil)
+        navigationItem.leftBarButtonItem = cancelItem
+        navigationItem.rightBarButtonItem = saveItem
+        navigationBar.setItems([navigationItem], animated: false)
+        return navigationBar
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        configureNavigationBar()
+        addSubview()
+        configureLayout()
+    }
+
+    private func addSubview() {
+        view.addSubview(newContactNavigationBar)
     }
     
-    private func configureNavigationBar() {
-        let navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 44))
-        view.addSubview(navigationBar)
-        
-        let navigationItem = UINavigationItem(title: "새 연락처")
-        let cancelItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: nil)
-        let saveItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: nil)
-        navigationItem.leftBarButtonItem = cancelItem
-        navigationItem.rightBarButtonItem = saveItem
-        
-        navigationBar.setItems([navigationItem], animated: false)
+    private func configureLayout() {
+        let safeArea = view.safeAreaLayoutGuide
+
+        newContactNavigationBar.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            newContactNavigationBar.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            newContactNavigationBar.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            newContactNavigationBar.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            newContactNavigationBar.widthAnchor.constraint(equalTo: view.widthAnchor),
+        ])
     }
 
 }
