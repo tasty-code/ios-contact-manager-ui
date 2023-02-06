0db07e9c1ef8ef8a4f419028c4ebf7d931fc288d
qwerty3345 <qwerty3345@naver.com>
2023-02-06 13:15:01 +0900

    feat: 연락처 추가 화면 취소 버튼 기능 구현
    - 유저에게 얼럿을 띄우고 정말로 취소할지 확인하게 함

--- ContactManager/ContactManagerUI/ViewController/AddContactViewController.swift
+++ ContactManager/ContactManagerUI/ViewController/AddContactViewController.swift
@@ -16,5 +16,16 @@ class AddContactViewController: UIViewController {
         navigationBar.shadowImage = UIImage()
     }
     
+    @IBAction func tappedCancelButton(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "정말로 취소하시겠습니까?", message: nil, preferredStyle: .alert)
+        let noAction = UIAlertAction(title: "아니오", style: .cancel)
+        let yesAction = UIAlertAction(title: "예", style: .destructive) { _ in self.dismiss(animated: true)
+        }
+
+        alert.addAction(noAction)
+        alert.addAction(yesAction)
+
+        present(alert, animated: true)
+    }
 
 }
