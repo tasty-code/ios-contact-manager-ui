731b7319732d92096c49c82913540780cf1237cf
longlivedrgn <kimye9808@naver.com>
2023-02-06 10:36:48 +0900

    feat: label, textfield, stackview 추가 및 레이아웃 설정

--- contactManager/ContactManagerUI/AddContactViewController.swift
+++ contactManager/ContactManagerUI/AddContactViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class AddContactViewController: UIViewController {
-
+    
     var newContactNavigationBar: UINavigationBar = {
         let navigationBar = UINavigationBar()
         let navigationItem = UINavigationItem(title: "새 연락처")
@@ -19,21 +19,103 @@ class AddContactViewController: UIViewController {
         navigationBar.setItems([navigationItem], animated: false)
         return navigationBar
     }()
-
+    
+    var nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "이름"
+        return label
+    }()
+    
+    var ageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "나이"
+        return label
+    }()
+    
+    var phoneNumberLabel: UILabel = {
+        let label = UILabel()
+        label.text = "연락처"
+        return label
+    }()
+    
+    var nameTextField: UITextField = {
+        let textField = UITextField()
+        return textField
+    }()
+    
+    var ageTextField: UITextField = {
+        let textField = UITextField()
+        return textField
+    }()
+    
+    var phoneNumberTextField: UITextField = {
+        let textField = UITextField()
+        return textField
+    }()
+    
+    var nameStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 8
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    var ageStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 8
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    var phoneNumberStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 8
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    var contactInfoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 20
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         addSubview()
         configureLayout()
     }
-
+    
     private func addSubview() {
         view.addSubview(newContactNavigationBar)
+        view.addSubview(contactInfoStackView)
+        contactInfoStackView.addArrangedSubview(nameStackView)
+        contactInfoStackView.addArrangedSubview(ageStackView)
+        contactInfoStackView.addArrangedSubview(phoneNumberStackView)
+        
+        nameStackView.addArrangedSubview(nameLabel)
+        nameStackView.addArrangedSubview(nameTextField)
+        
+        ageStackView.addArrangedSubview(ageLabel)
+        ageStackView.addArrangedSubview(ageTextField)
+        
+        phoneNumberStackView.addArrangedSubview(phoneNumberLabel)
+        phoneNumberStackView.addArrangedSubview(phoneNumberTextField)
     }
     
     private func configureLayout() {
         let safeArea = view.safeAreaLayoutGuide
-
+        
         newContactNavigationBar.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             newContactNavigationBar.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
@@ -41,6 +123,12 @@ class AddContactViewController: UIViewController {
             newContactNavigationBar.topAnchor.constraint(equalTo: safeArea.topAnchor),
             newContactNavigationBar.widthAnchor.constraint(equalTo: view.widthAnchor),
         ])
+        
+        contactInfoStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            contactInfoStackView.topAnchor.constraint(equalTo: newContactNavigationBar.bottomAnchor, constant: 20),
+            contactInfoStackView.leadingAnchor.constraint(equalTo:safeArea.leadingAnchor, constant: 20),
+            contactInfoStackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: 20),
+        ])
     }
-
 }
