e2eba7916f38399e9870c6dd640cc17920cf5b66
doshkor <dosh.kor@gmail.com>
2023-02-06 10:55:08 +0900

    feat: 연락처 추가 화면 추가

---
+++ ContactManager/ContactManagerUI/ViewController/AddContactViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddContactViewController.swift
+//  ContactManagerUI
+//
+//  Created by 신동오 on 2023/02/06.
+//
+
+import UIKit
+
+class AddContactViewController: UIViewController {
+    
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationBar.shadowImage = UIImage()
+    }
+    
+
+}
