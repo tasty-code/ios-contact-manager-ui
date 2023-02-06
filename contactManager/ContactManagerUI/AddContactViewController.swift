f4b53328984573198630e8cc570b5f5c7bfd85c0
longlivedrgn <kimye9808@naver.com>
2023-02-06 10:20:09 +0900

    feat: AddContactViewController 내 navigation bar 추가
    - AddContactViewController는 스토리보드 대신 코드로 진행

--- contactManager/ContactManagerUI/AddContactViewController.swift
+++ contactManager/ContactManagerUI/AddContactViewController.swift
@@ -11,8 +11,21 @@ class AddContactViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .gray
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        configureNavigationBar()
+    }
+    
+    private func configureNavigationBar() {
+        let navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 44))
+        view.addSubview(navigationBar)
+        
+        let navigationItem = UINavigationItem(title: "새 연락처")
+        let cancelItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: nil)
+        let saveItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: nil)
+        navigationItem.leftBarButtonItem = cancelItem
+        navigationItem.rightBarButtonItem = saveItem
+        
+        navigationBar.setItems([navigationItem], animated: false)
     }
 
 }
