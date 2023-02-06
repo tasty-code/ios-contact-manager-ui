9364ad4de11dc928dcfef9298b91f4ba72b539de
bomyuniverse <71758542+calledBlu@users.noreply.github.com>
2023-02-06 16:22:39 +0900

    Feat: Error 타입 정의

---
+++ ContactManager/ContactManager/Model/Errors.swift
@@ -0,0 +1,14 @@
+//
+//  Errors.swift
+//  ContactManager
+//
+//  Created by Blu on 2023/02/06.
+//
+
+import Foundation
+
+enum Errors: Error {
+    case notFoundJsonFile
+    case notLoadData
+    case unableToParse
+}
