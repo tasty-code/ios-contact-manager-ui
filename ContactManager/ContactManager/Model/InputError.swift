e75f1f8d48acf4baef9940acb269c37716cdd503
qwerty3345 <qwerty3345@naver.com>
2023-02-06 14:42:58 +0900

    feat: 사용자 입력 검증 및 알림 로직 구현
    - 연락처 정보로 변환하지 못할 경우, 해당 실패 케이스에 따라 유저에게 얼럿을 띄워줌

--- ContactManager/ContactManager/Model/InputError.swift
+++ ContactManager/ContactManager/Model/InputError.swift
@@ -22,13 +22,13 @@ enum InputError: LocalizedError {
         case .emptyInput:
             return "아무것도 입력되지 않았습니다. 입력 형식을 확인해주세요."
         case .invalidName:
-            return "입력한 이름이 잘못되었습니다. 입력 형식을 확인해주세요"
+            return "입력한 이름 정보가 잘못되었습니다"
         case .invalidInput:
             return "입력이 올바르지 않습니다."
         case .invalidAge:
-            return "입력한 나이정보가 잘못되었습니다. 입력 형식을 확인해주세요."
+            return "입력한 나이 정보가 잘못되었습니다"
         case .invalidPhoneNumber:
-            return "입력한 연락처정보가 잘못되었습니다. 입력 형식을 확인해 주세요."
+            return "입력한 연락처 정보가 잘못되었습니다"
         }
     }
 }
--- ContactManager/ContactManagerUI/ViewController/AddContactViewController.swift
+++ ContactManager/ContactManagerUI/ViewController/AddContactViewController.swift
@@ -31,6 +31,38 @@ final class AddContactViewController: UIViewController {
 
     // MARK: - Actions
     @IBAction private func tappedCancelButton(_ sender: UIBarButtonItem) {
+        showCheckCancelAlert()
+    }
+    
+    @IBAction private func tappedSaveButton(_ sender: UIBarButtonItem) {
+        guard let name = nameTextField.text ,
+              let age = ageTextField.text,
+              let phoneNumber = phoneNumberTextField.text else { return }
+
+        let inputArray = [name, age, phoneNumber]
+
+        do {
+            let contact = try InputManager.contact(from: inputArray)
+            ContactManager.shared.add(contact: contact)
+        } catch {
+            showFailAlert(withTitle: error.localizedDescription)
+            return
+        }
+
+        dismiss(animated: true)
+        delegate?.didContactChanged()
+    }
+
+    // MARK: - Helpers
+    private func showFailAlert(withTitle title: String) {
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: "확인", style: .default)
+
+        alert.addAction(confirmAction)
+        present(alert, animated: true)
+    }
+
+    private func showCheckCancelAlert() {
         let alert = UIAlertController(title: "정말로 취소하시겠습니까?", message: nil, preferredStyle: .alert)
         let noAction = UIAlertAction(title: "아니오", style: .cancel)
         let yesAction = UIAlertAction(title: "예", style: .destructive) { _ in self.dismiss(animated: true)
@@ -40,17 +72,6 @@ final class AddContactViewController: UIViewController {
         alert.addAction(yesAction)
 
         present(alert, animated: true)
-    }
-    
-    @IBAction private func tappedSaveButton(_ sender: UIBarButtonItem) {
-        guard let name = nameTextField.text ,
-              let age = Int(ageTextField.text ?? ""),
-              let phoneNumber = phoneNumberTextField.text else { return }
-        let contact = Contact(name: name, age: age, phoneNumber: phoneNumber)
-        ContactManager.shared.add(contact: contact)
-        
-        dismiss(animated: true)
-        delegate?.didContactChanged()
     }
 }
 
@@ -66,6 +87,10 @@ extension AddContactViewController: UITextFieldDelegate {
     }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard !string.isEmpty else {
+            return true
+        }
+
         if textField == self.ageTextField {
             guard let _ = Int(string) else {
                 return false
