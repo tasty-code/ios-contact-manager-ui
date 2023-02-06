8e419b1aabbe0806fbbb109c71677bf436f2147a
Sei Kim <vanism7557@gmail.com>
2023-02-06 14:27:22 +0900

    feat: AddContactView View Controller 추가

---
+++ ios-contact-manager-ui/ContactManagerUI/AddContactViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddContactViewController.swift
+//  ContactManagerUI
+//
+//  Created by sei_dev on 2/6/23.
+//
+
+import UIKit
+
+final class AddContactViewController: UIViewController {
+
+    @IBOutlet weak var contactTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        contactTextField.delegate = self
+    }
+}
+
+extension AddContactViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return true
+    }
+}
