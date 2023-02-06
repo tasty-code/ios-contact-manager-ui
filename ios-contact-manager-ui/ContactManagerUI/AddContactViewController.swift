69b5763cb8aaec3b9f7228c84b58629c84845b59
Sei Kim <vanism7557@gmail.com>
2023-02-06 17:24:49 +0900

    feat: 연락처 hyphen 처리 함수 구현 test

--- ios-contact-manager-ui/ContactManagerUI/AddContactViewController.swift
+++ ios-contact-manager-ui/ContactManagerUI/AddContactViewController.swift
@@ -19,6 +19,47 @@ final class AddContactViewController: UIViewController {
 
 extension AddContactViewController: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let oldText = textField.text {
+            let formattedString = parse(target: oldText, range: range)
+            textField.text = formattedString
+        }
         return true
+    }
+
+    private func parse(target: String, range: NSRange) -> String {
+        let suffix: String
+
+        switch (range.location, range.length) {
+        case (12, 1...):
+            return addHyphen(to: slicingFromStart(target: target, offset: range.location), at: (2, 7))
+        case (11, 1...):
+            return addHyphen(to: slicingFromStart(target: target, offset: range.location), at: (2, 6))
+        case (11, 0):
+            return addHyphen(to: target, at: (2, 7))
+        case (12, 0):
+            return addHyphen(to: target, at: (3, 8))
+        case (3, 1...), (7, 1...):
+            return removeHyphen(target: target, length: range.location)
+        case (2, _), (6, _):
+            suffix = "-"
+        default:
+            suffix = ""
+        }
+        return target + suffix
+    }
+
+    private func slicingFromStart(target:String, offset length: Int) -> String {
+        return String(target[...target.index(target.startIndex, offsetBy: length)])
+    }
+
+    private func removeHyphen(target: String, length: Int) -> String {
+        return slicingFromStart(target: target, offset: length-1)
+    }
+
+    private func addHyphen(to string: String, at index: (first: Int, second: Int)) -> String {
+        var hyphenRemoved = string.replacingOccurrences(of: "-", with: "")
+        hyphenRemoved.insert("-", at: hyphenRemoved.index(hyphenRemoved.startIndex, offsetBy: index.first))
+        hyphenRemoved.insert("-", at: hyphenRemoved.index(hyphenRemoved.startIndex, offsetBy: index.second))
+        return hyphenRemoved
     }
 }
