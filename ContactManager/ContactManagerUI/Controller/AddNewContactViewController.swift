bbb05c34442c089b04614bf0768e97d171858f04
bomyuniverse <71758542+calledBlu@users.noreply.github.com>
2023-02-07 10:23:18 +0900

    Feat: AddNewContactViewController 추가

---
+++ ContactManager/ContactManagerUI/Controller/AddNewContactViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddNewContactViewController.swift
+//  ContactManagerUI
+//
+//  Created by Blu on 2023/02/07.
+//
+
+import UIKit
+
+class AddNewContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
