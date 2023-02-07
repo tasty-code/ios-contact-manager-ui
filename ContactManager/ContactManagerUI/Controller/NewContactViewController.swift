5eff78eacb5bed949570bdfa64e4c98eaa31dc8c
ohdair <ohdiar@gmail.com>
2023-02-07 11:05:17 +0900

    feat: 전화번호 입력시 주어진 형식에 맞게 변경
    - 주어진 형식에 맞게 String을 변환해주는 format() 메서드 추가
    - UITextFieldDelegate를 사용하여 textField의 text를 변경

--- ContactManager/ContactManagerUI/Controller/NewContactViewController.swift
+++ ContactManager/ContactManagerUI/Controller/NewContactViewController.swift
@@ -8,9 +8,11 @@
 import UIKit
 
 final class NewContactViewController: UIViewController {
+    @IBOutlet weak var phoneNumberTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        phoneNumberTextField.delegate = self
     }
 
     @IBAction func cancelButtonDidTap(_ sender: UIBarButtonItem) {
@@ -22,5 +24,46 @@ final class NewContactViewController: UIViewController {
         })
         alert.addAction(yesButton)
         present(alert, animated: true)
+    }
+}
+
+extension NewContactViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text else { return false }
+        let newString = (text as NSString).replacingCharacters(in: range, with: string)
+        textField.text = format(phone: newString)
+        return false
+    }
+
+    private func format(phone: String) -> String {
+        let numbers = phone.filter({ $0.isNumber })
+        var index = numbers.startIndex
+        var formatType = ""
+        var formattedNumbers = ""
+
+        switch numbers.count {
+        case ...9:
+            formatType = "XX-XXX-XXXX"
+        case 10:
+            formatType = "XXX-XXX-XXXX"
+        default:
+            formatType = "XXX-XXXX-XXXX"
+        }
+
+        for character in formatType where index < numbers.endIndex {
+            if character == "X" {
+                formattedNumbers.append(numbers[index])
+                index = numbers.index(after: index)
+            } else {
+                formattedNumbers.append(character)
+            }
+        }
+
+        while index < numbers.endIndex {
+            formattedNumbers.append(numbers[index])
+            index = numbers.index(after: index)
+        }
+
+        return formattedNumbers
     }
 }
