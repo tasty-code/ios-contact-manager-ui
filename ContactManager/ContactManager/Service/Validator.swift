444843dad007eb2cefb9bf20fd24071761ed2d32
devxsby <devxsby@gmail.com>
2023-02-07 16:04:41 +0900

    fix: phone number 유효 카운트 검출 수 변경

--- ContactManager/ContactManager/Service/Validator.swift
+++ ContactManager/ContactManager/Service/Validator.swift
@@ -43,6 +43,6 @@ struct Validator: ValidatorProtocol {
     
     private func validateNumber(_ number: String) throws {
         let numberSplit = number.split(separator: "-").map { String($0) }
-        if number.count < 10 || numberSplit.count < 3 { throw Errors.wrongPhoneNumber }
+        if number.count < 11 || numberSplit.count < 3 { throw Errors.wrongPhoneNumber }
     }
 }
