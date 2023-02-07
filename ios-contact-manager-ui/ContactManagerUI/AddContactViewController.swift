c9e4ec20000172af8c1676a57d230f9342d8b2c1
sei <vanism7557@gmail.com>
2023-02-07 10:39:51 +0900

    feat: Hyphen 관련 refactoring

--- ios-contact-manager-ui/ContactManagerUI/AddContactViewController.swift
+++ ios-contact-manager-ui/ContactManagerUI/AddContactViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 final class AddContactViewController: UIViewController {
-
     @IBOutlet weak var contactTextField: UITextField!
 
     override func viewDidLoad() {
@@ -19,47 +18,69 @@ final class AddContactViewController: UIViewController {
 
 extension AddContactViewController: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        if let oldText = textField.text {
-            let formattedString = parse(target: oldText, range: range)
-            textField.text = formattedString
+        if let oldText = textField.text, let textRange = Range(range, in: oldText) {
+            // newText == 현재 text field에 있는 텍스트. 아직 textField에는 업데이트 되지 않은 상태
+            let newText = oldText.replacingCharacters(in: textRange, with: string)
+
+            // range 정보를 갖고 parse한 결과를 textField의 text에 할당
+            textField.text = parse(newText, using: range)
         }
-        return true
+        return false
     }
 
-    private func parse(target: String, range: NSRange) -> String {
-        let suffix: String
-
+    private func parse(_ newText: String, using range: NSRange) -> String {
+        /*
+         range.location == newText.count
+         range.length == 삭제된 character 수
+         */
         switch (range.location, range.length) {
-        case (12, 1...):
-            return addHyphen(to: slicingFromStart(target: target, offset: range.location), at: (2, 7))
-        case (11, 1...):
-            return addHyphen(to: slicingFromStart(target: target, offset: range.location), at: (2, 6))
-        case (11, 0):
-            return addHyphen(to: target, at: (2, 7))
-        case (12, 0):
-            return addHyphen(to: target, at: (3, 8))
-        case (3, 1...), (7, 1...):
-            return removeHyphen(target: target, length: range.location)
-        case (2, _), (6, _):
-            suffix = "-"
-        default:
-            suffix = ""
+        case (12, 1...), (11, 0): // 2-4-4로 바뀌어야 하는 경우
+            return relocateHyphen(of: newText, locationsOfHyphen: [2, 7])
+        case (11, 1...): // 2-3-4로 바뀌어야 하는 경우
+            return relocateHyphen(of: newText, locationsOfHyphen: [2, 6])
+        case (12, 0): // 3-4-4로 바뀌어야 하는 경우
+            return relocateHyphen(of: newText, locationsOfHyphen: [3, 8])
+        case (3, 1...), (7, 1...): // 맨 뒤 -를 없애는 경우
+            return removeLastHyphen(from: newText)
+        case (2, _), (6, _): // -를 추가해야 하는 경우
+            return newText.inserting(Character.hyphen, at: range.location)
+        default: // 그 외의 경우
+            return newText
         }
-        return target + suffix
     }
 
-    private func slicingFromStart(target:String, offset length: Int) -> String {
-        return String(target[...target.index(target.startIndex, offsetBy: length)])
+    private func removeLastHyphen(from text: String) -> String {
+        if text.last == Character.hyphen {
+            return String(text.dropLast())
+        }
+        return text
     }
 
-    private func removeHyphen(target: String, length: Int) -> String {
-        return slicingFromStart(target: target, offset: length-1)
+    private func relocateHyphen(of string: String, locationsOfHyphen indices: [Int]) -> String {
+        var relocated = string.replacingOccurrences(of: String.hyphen, with: String.empty)
+        for index in indices {
+            relocated.insert(Character.hyphen, at: index)
+        }
+        return relocated
+    }
+}
+
+
+extension String {
+    static var hyphen: String { "-" }
+    static var empty: String { "" }
+
+    mutating func insert(_ newElement: Character, at i: Int) {
+        self.insert(newElement, at: self.index(self.startIndex, offsetBy: i))
     }
 
-    private func addHyphen(to string: String, at index: (first: Int, second: Int)) -> String {
-        var hyphenRemoved = string.replacingOccurrences(of: "-", with: "")
-        hyphenRemoved.insert("-", at: hyphenRemoved.index(hyphenRemoved.startIndex, offsetBy: index.first))
-        hyphenRemoved.insert("-", at: hyphenRemoved.index(hyphenRemoved.startIndex, offsetBy: index.second))
-        return hyphenRemoved
+    func inserting(_ newElement: Character, at i: Int) -> String {
+        var newString = self
+        newString.insert(newElement, at: self.index(self.startIndex, offsetBy: i))
+        return newString
     }
+}
+
+extension Character {
+    static var hyphen: Character { "-" }
 }
