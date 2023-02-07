7783238d5d682f36ff4c6580df01fbca08ac242a
qwerty3345 <qwerty3345@naver.com>
2023-02-07 15:31:41 +0900

    refactor: 전화번호 Formatting 관련 String Extension 메서드 TextField 커스텀 클래스로 이동

--- ContactManager/ContactManagerUI/View/PhoneNumberTextField.swift
+++ ContactManager/ContactManagerUI/View/PhoneNumberTextField.swift
@@ -28,15 +28,64 @@ final class PhoneNumberTextField: UITextField {
 
     // MARK: - Actions
     @objc func didPhoneNumberTextChange() {
-        text = text?.formattedPhoneNumber
+        text = formattedPhoneNumber(with: text)
     }
 }
 
+// MARK: - UITextFieldDelegate
 extension PhoneNumberTextField: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         guard string.isEmpty || Int(string) != nil else {
             return false
         }
         return true
+    }
+}
+
+// MARK: - Number Format 관련
+extension PhoneNumberTextField {
+    func formattedPhoneNumber(with text: String?) -> String {
+        let pureNumbers = text?.filter { $0.isNumber } ?? ""
+        var pattern = ""
+        var result: String {
+            return applyPatternOn(pureNumbers: pureNumbers, pattern: pattern, replacementCharacter: "#")
+        }
+
+        if pureNumbers.starts(with: "0") && !pureNumbers.starts(with: "02")  {
+            if pureNumbers.count <= 10 {
+                pattern = "###-###-####"
+            }
+            if pureNumbers.count == 11 {
+                pattern = "###-####-####"
+            }
+            return result
+        }
+
+        if pureNumbers.count <= 9 {
+            pattern = "##-###-####"
+            return result
+        }
+
+        if pureNumbers.count == 10 {
+            pattern = "##-####-####"
+            return result
+        }
+
+        return result
+    }
+
+    private func applyPatternOn(pureNumbers: String,
+                                pattern: String,
+                                replacementCharacter: Character) -> String {
+        var numbers = pureNumbers
+        for index in 0 ..< pattern.count {
+            guard index < numbers.count else { return numbers }
+            let pattenIndex = pattern.index(pattern.startIndex, offsetBy: index)
+            let patternCharacter = pattern[pattenIndex]
+
+            guard patternCharacter != replacementCharacter else { continue }
+            numbers.insert(patternCharacter, at: pattenIndex)
+        }
+        return numbers
     }
 }
