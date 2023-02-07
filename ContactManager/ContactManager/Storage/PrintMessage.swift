f7fb52a108cec6075b4bdef5ce804993290ec6a6
Jason <ggj00125@gmail.com>
2023-02-07 17:35:53 +0900

    Refactor: UserInput에 관련된 ErrorMessage 분리

--- ContactManager/ContactManager/Storage/PrintMessage.swift
+++ ContactManager/ContactManager/Storage/PrintMessage.swift
@@ -11,11 +11,8 @@ enum PrintMessage: String, CustomStringConvertible {
     case startComment = "1) 연락처 추가 2) 연락처 목록보기 3) 연락처 검색 x) 종료\n메뉴를 선택해주세요 : "
     case requestContactInformation = "연락처 정보를 입력해주세요 : "
     case correctContactInformation = "입력한 정보는 "
-    case requestToSearchName = "연락처에서 찾을 이름을 입력해주세요 : "
-    case invalidName = "입력한 이름 정보가 잘못되었습니다. 입력 형식을 확인해주세요.\n"
-    case invalidAge = "입력한 나이 정보가 잘못되었습니다. 입력 형식을 확인해주세요.\n"
-    case invalidPhoneNumber = "입력한 연락처 정보가 잘못되었습니다. 입력 형식을 확인해주세요.\n"
     case invalidFormat = "입력한 정보의 형식이 맞지않습니다. 올바른 형식을 확인해주세요.\n"
+    case requestToSearchName = "연락처에서 찾을 이름을 입력해주세요 : "
     case nothingUserInput = "아무것도 입력되지 않았습니다. 입력 형식을 확인해주세요.\n"
     case choiceWrorngMenu = "선택이 잘못되었습니다 확인 후 다시 입력해주세요.\n"
     case exitProgram = "[프로그램 종료]"
@@ -46,6 +43,16 @@ enum PrintMessage: String, CustomStringConvertible {
             print("- \(element.name) / \(element.age) / \(element.phoneNumber)")
         }
     }
+    
+    var description: String {
+        return self.rawValue
+    }
+}
+
+enum InputErrorMessage: String, CustomStringConvertible, CaseIterable {
+    case invalidName = "입력한 이름 정보가 잘못되었습니다. 입력 형식을 확인해주세요.\n"
+    case invalidAge = "입력한 나이 정보가 잘못되었습니다. 입력 형식을 확인해주세요.\n"
+    case invalidPhoneNumber = "입력한 연락처 정보가 잘못되었습니다. 입력 형식을 확인해주세요.\n"
     
     var description: String {
         return self.rawValue
