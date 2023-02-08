ac86b4b6a0f86b04227419bac0441584bf05bf25
Jason <ggj00125@gmail.com>
2023-02-08 10:29:43 +0900

    Refactor: Alert 영역 파일로 분리 및 접근제어 internal로 변경

--- ContactManager/ContactManagerUI/Controller/AddNewContactViewController.swift
+++ ContactManager/ContactManagerUI/Controller/AddNewContactViewController.swift
@@ -74,27 +74,3 @@ class AddNewContactViewController: UIViewController {
         return resultArray
     }
 }
-
-extension AddNewContactViewController {
-    private func successAlert(message: String) {
-        let success = UIAlertAction(title: "확인", style: .default, handler: nil)
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        
-        alert.addAction(success)
-
-        present(alert, animated: true, completion: nil)
-    }
-    
-    private func cancelConfirmAlert() {
-        let alert = UIAlertController(title: nil, message: "정말로 취소하시겠습니까?", preferredStyle: .alert)
-        let allowAction = UIAlertAction(title: "예", style: .destructive, handler: { action in
-            self.dismiss(animated: true)
-        })
-        let cancleAction = UIAlertAction(title: "아니오", style: .cancel, handler: nil)
-        
-        alert.addAction(allowAction)
-        alert.addAction(cancleAction)
-
-        present(alert, animated: true, completion: nil)
-    }
-}
