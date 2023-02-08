938e2bc46c7263ce503b397c410f75eb7a882283
bomyuniverse <71758542+calledBlu@users.noreply.github.com>
2023-02-08 09:49:49 +0900

    Refactor: 이름을 검사하는 정규식 리팩토링
    - 기존에 최소 글자 수 제한이 없어서 아무것도 입력하지 않아도 통과되는 문제를 해결
    - 1글자 이상으로 최소 글자수를 제한하도록 변경

--- ContactManager/ContactManager/Storage/RegularExpression.swift
+++ ContactManager/ContactManager/Storage/RegularExpression.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum RegularExpression: String, CustomStringConvertible, CaseIterable {
-    case namePattern = "^[A-Za-z]*$"
+    case namePattern = "^[A-Za-z]{1,}$"
     case agePattern = "^[0-999]{1,3}$"
     case phoneNumberPattern = "^0([0-9]{1,2})-([0-9]{3,4})-([0-9]{4})*$"
     
