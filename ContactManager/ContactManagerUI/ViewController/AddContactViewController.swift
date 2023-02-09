d593c7de9c24bca87dcf4bd6f6c9087442924603
qwerty3345 <qwerty3345@naver.com>
2023-02-09 11:18:53 +0900

    refactor: TextField View가 데이터를 수정하지 않도록 MVC 구조로 변경

--- ContactManager/ContactManagerUI/ViewController/AddContactViewController.swift
+++ ContactManager/ContactManagerUI/ViewController/AddContactViewController.swift
@@ -50,6 +50,10 @@ final class AddContactViewController: UIViewController {
         dismiss(animated: true)
         delegate?.didContactChanged()
     }
+
+    @objc func didPhoneNumberTextChange() {
+        phoneNumberTextField.text = formattedPhoneNumber(with: phoneNumberTextField.text)
+    }
     
     // MARK: - Helpers
     private func showFailAlert(withTitle title: String) {
@@ -75,6 +79,8 @@ final class AddContactViewController: UIViewController {
     private func configureTextField() {
         nameTextField.delegate = self
         ageTextField.delegate = self
+
+        phoneNumberTextField.addTarget(self, action: #selector(didPhoneNumberTextChange), for: .editingChanged)
     }
 }
 
@@ -82,11 +88,57 @@ extension AddContactViewController: UITextFieldDelegate {
     func textField(_ textField: UITextField,
                    shouldChangeCharactersIn range: NSRange,
                    replacementString string: String) -> Bool {
-        if textField == ageTextField {
+        if textField == ageTextField || textField == phoneNumberTextField {
             guard string.isEmpty || Int(string) != nil else {
                 return false
             }
         }
         return true
+    }
+}
+
+// MARK: - PhoneNumberTextField Format 관련
+extension AddContactViewController {
+    private func formattedPhoneNumber(with text: String?) -> String {
+        let pureNumbers = text?.filter { $0.isNumber } ?? ""
+        let pattern = createPhoneNumberPattern(with: pureNumbers)
+            return applyPatternOn(pureNumbers: pureNumbers, pattern: pattern, replacementCharacter: "#")
+    }
+
+    private func createPhoneNumberPattern(with pureNumbers: String) -> String {
+        if pureNumbers.starts(with: "0") && !pureNumbers.starts(with: "02")  {
+            if pureNumbers.count <= 10 {
+                return "###-###-####"
+            }
+
+            if pureNumbers.count == 11 {
+                return "###-####-####"
+            }
+        }
+
+        if pureNumbers.count <= 9 {
+            return "##-###-####"
+        }
+
+        if pureNumbers.count == 10 {
+            return "##-####-####"
+        }
+
+        return ""
+    }
+
+    private func applyPatternOn(pureNumbers: String,
+                                pattern: String,
+                                replacementCharacter: Character) -> String {
+        var numbers = pureNumbers
+        for index in 0 ..< pattern.count {
+            guard index < numbers.count else { return numbers }
+            let pattenIndex = pattern.index(pattern.startIndex, offsetBy: index)
+            let patternCharacter = pattern[pattenIndex]
+
+            guard patternCharacter != replacementCharacter else { continue }
+            numbers.insert(patternCharacter, at: pattenIndex)
+        }
+        return numbers
     }
 }
