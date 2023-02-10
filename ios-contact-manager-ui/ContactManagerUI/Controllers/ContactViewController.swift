55e72e82be2dae3231b7c12c43cb0ca453931eb5
sei <vanism7557@gmail.com>
2023-02-10 10:57:16 +0900

    feat: UITableViewDiffableDataSource 구현
    - UITableViewDiffableDataSource 를 위한 구현 사항들
    ContactViewController.Section 구조, setupDataSource() method
    - 기존의 TableViewDataSouce 삭제

--- ios-contact-manager-ui/ContactManagerUI/Controllers/ContactViewController.swift
+++ ios-contact-manager-ui/ContactManagerUI/Controllers/ContactViewController.swift
@@ -8,13 +8,17 @@
 import UIKit
 
 final class ContactViewController: UIViewController {
-
+    enum Section {
+        case main
+    }
+    private let modelData = ModelData()
     @IBOutlet private weak var tableView: UITableView!
-    private let dataSource = TableViewDataSource()
+    private var dataSource: UITableViewDiffableDataSource<Section, UserInfo>?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
+        setupDataSource()
     }
 
     private func setupTableView() {
@@ -22,6 +26,29 @@ final class ContactViewController: UIViewController {
         tableView.dataSource = dataSource
     }
 }
+
+extension ContactViewController {
+    private func setupDataSource() {
+        dataSource = UITableViewDiffableDataSource<Section, UserInfo>(tableView: tableView) { tableView, indexPath, itemIdentifier in
+            let identifier = "contactCell"
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: identifier) else {
+                fatalError("cannot provide cell..")
+            }
+            var content = cell.defaultContentConfiguration()
+            content.text = itemIdentifier.text
+            content.secondaryText = itemIdentifier.secondaryText
+            cell.contentConfiguration = content
+            return cell
+        }
+
+        let contacts = modelData.contacts
+        var snapshot = NSDiffableDataSourceSnapshot<Section, UserInfo>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(contacts)
+        dataSource?.apply(snapshot, animatingDifferences: false)
+    }
+}
+
 
 extension ContactViewController: UITableViewDelegate {
 
