98dea1ff35c3b05bd67300be033eb005ad7bf3f4
ohdair <ohdiar@gmail.com>
2023-02-13 17:58:03 +0900

    style: Trailing Closures 형식으로 변경

--- ContactManager/ContactManagerUI/Controller/NewContactViewController.swift
+++ ContactManager/ContactManagerUI/Controller/NewContactViewController.swift
@@ -24,9 +24,9 @@ final class NewContactViewController: UIViewController {
         let alert = UIAlertController(title: "정말로 취소하시겠습니까?", message: nil, preferredStyle: .alert)
         let noButton = UIAlertAction(title: "아니오", style: .default)
         alert.addAction(noButton)
-        let yesButton = UIAlertAction(title: "예", style: .destructive, handler: { _ in
+        let yesButton = UIAlertAction(title: "예", style: .destructive) { _ in
             self.dismiss(animated: true)
-        })
+        }
         alert.addAction(yesButton)
         present(alert, animated: true)
     }
