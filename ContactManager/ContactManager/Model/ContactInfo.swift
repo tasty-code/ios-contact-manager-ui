0abee66c50502f4f9bae14a20891a4b9664177ae
janine-dev <developerjanine@gmail.com>
2023-10-04 17:26:24 +0900

    feat ContactInfo 모델 초기 세팅

---
+++ ContactManager/ContactManager/Model/ContactInfo.swift
@@ -0,0 +1,20 @@
+//
+//  ContactInfo.swift
+//  ContactManager
+//
+//  Created by Janine on 2023/10/04.
+//
+
+import Foundation
+
+struct ContactInfo {
+    var name: String?
+    var age: Int?
+    var phoneNum: String?
+    
+    init(name: String, age: Int, phoneNum: String) {
+        self.name = name
+        self.age = age
+        self.phoneNum = phoneNum
+    }
+}
