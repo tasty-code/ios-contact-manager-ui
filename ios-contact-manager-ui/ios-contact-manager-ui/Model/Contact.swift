58d352c20bc4ce6c32d1e7bc68bbcf2e5f82b9cb
bamsak <qjatjrdl963@naver.com>
2023-10-04 11:47:06 +0900

    [Feat] Contact의 CRUD 메소드 생성

--- ios-contact-manager-ui/ios-contact-manager-ui/Model/Contact.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Model/Contact.swift
@@ -7,17 +7,32 @@
 
 import Foundation
 
-
+typealias PhoneNumber = String
 
 struct Contact: Hashable {
     let uuid = UUID()
     private(set) var name: String
     private(set) var age: Int
     private(set) var phoneNumbers = Set<PhoneNumber>()
+    
+    fileprivate mutating func changeName(_ name: String) {
+        self.name = name
+    }
+    
+    fileprivate mutating func changeAge(_ age: Int) {
+        self.age = age
+    }
+    
+    fileprivate mutating func add(_ phoneNumber: PhoneNumber) {
+        phoneNumbers.insert(phoneNumber)
+    }
+    
+    fileprivate mutating func delete(_ phoneNumber: PhoneNumber) {
+        phoneNumbers.remove(phoneNumber)
+    }
+    
+    fileprivate mutating func modify(from existingPhoneNumber: PhoneNumber, to newPhoneNumber: PhoneNumber) {
+        phoneNumbers.remove(existingPhoneNumber)
+        phoneNumbers.insert(newPhoneNumber)
+    }
 }
-
-
-typealias PhoneNumber = String
-
-
-
