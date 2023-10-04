fe67a06873d5fa5ac554d724c21fed26ee52dc8c
COMDORI <02145s1@gmail.com>
2023-10-04 16:21:18 +0900

    config 프로젝트 초기 설정

---
+++ ContactManager/ContactManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactManager
+//
+//  Created by Wonji Ha on 2023/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
