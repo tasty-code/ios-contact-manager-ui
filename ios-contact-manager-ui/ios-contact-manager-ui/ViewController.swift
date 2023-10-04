59103a8f7d5eb021e40701e27f0d75d5c14c74d4
kimjunsung <assd1108@naver.com>
2023-10-04 09:42:33 +0900

    [Config] 프로젝트 생성

---
+++ ios-contact-manager-ui/ios-contact-manager-ui/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-contact-manager-ui
+//
+//  Created by 김준성 on 2023/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
