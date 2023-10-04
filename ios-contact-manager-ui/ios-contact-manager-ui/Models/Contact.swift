372eef953e856f6ec654de77d8e7cde8005865ae
JinUng41 <ingjwjw@naver.com>
2023-10-04 16:24:52 +0900

    config: 프로젝트 생성 및 모델 정의

---
+++ ios-contact-manager-ui/ios-contact-manager-ui/Models/Contact.swift
@@ -0,0 +1,20 @@
+//
+//  Contact.swift
+//  ios-contact-manager-ui
+//
+//  Created by 김진웅 on 2023/10/04.
+//
+
+import Foundation
+
+class Contact {
+    var name: String
+    var age: Int
+    var phoneNumber: String
+    
+    init(name: String, age: Int, phoneNumber: String) {
+        self.name = name
+        self.age = age
+        self.phoneNumber = phoneNumber
+    }
+}
---
+++ ios-contact-manager-ui/ios-contact-manager-ui/Models/ContactManager.swift
@@ -0,0 +1,28 @@
+//
+//  ContactManager.swift
+//  ios-contact-manager-ui
+//
+//  Created by 김진웅 on 2023/10/04.
+//
+
+import Foundation
+
+class ContactManager {
+    private var contacts: [Contact] = []
+    
+    var count: Int {
+        return contacts.count
+    }
+    
+    func addContact(name: String, age: Int, phoneNumber: String) {
+        contacts.append(Contact(name: name, age: age, phoneNumber: phoneNumber))
+    }
+    
+    func deleteContact(index: Int) {
+        contacts.remove(at: index)
+    }
+    
+    func showContact(index: Int) -> Contact {
+        return contacts[index]
+    }
+}
