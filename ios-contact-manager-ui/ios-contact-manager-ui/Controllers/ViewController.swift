f58fcf2d7e4694b2d2d02da3387035324350613f
DongJun96 <djlee1201@naver.com>
2023-10-05 10:22:36 +0900

    feat: cell에 데이터를 매핑

--- ios-contact-manager-ui/ios-contact-manager-ui/Controllers/ViewController.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Controllers/ViewController.swift
@@ -28,7 +28,11 @@ extension ViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath) as! ContactCell
+        let contact = contactManager.showContact(index: indexPath.row)
+        cell.titleLabel.text = "\(contact.name)(\(contact.age))"
+        cell.subtitleLabel.text = "\(contact.phoneNumber)"
+        cell.accessoryType = .disclosureIndicator
         
         return cell
     }
