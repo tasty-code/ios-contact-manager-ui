455119914d5bc2343fee15fda869d613c843ac98
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2023-10-05 11:20:19 +0900

    feat: model 구현

---
+++ ios-contact-manager-ui/ios-contact-manager-ui/Mode/PersonInformation.swift
@@ -0,0 +1,14 @@
+//
+//  PersonInformation.swift
+//  ios-contact-manager-ui
+//
+//  Created by 김수경 on 2023/10/05.
+//
+
+import Foundation
+
+struct PersonInformation {
+    var name: String
+    var contacts: String
+    var age: Int
+}
