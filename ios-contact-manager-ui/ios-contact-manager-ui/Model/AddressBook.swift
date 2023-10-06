10b73a16f9ed316d38ea36b8b187ab3de655be8f
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2023-10-06 18:13:19 +0900

    feat: tableview 추가 및 모델과 연결

--- ios-contact-manager-ui/ios-contact-manager-ui/Model/AddressBook.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Model/AddressBook.swift
@@ -10,7 +10,7 @@ import Foundation
 class AddressBook {
     private var contacts: [[Contact]] = Array(repeating: [], count: 26)
     
-    private func getFirstLetterIndex(_ name: String) -> Int {
+    func getFirstLetterIndex(_ name: String) -> Int {
         let firstLetter = Array(name)[0].uppercased()
         let index = firstLetter.unicodeScalars.map { Int($0.value)}[0]
         return Int(index - 65)
@@ -27,11 +27,23 @@ class AddressBook {
     }
     
     func changeContact(_ section: Int, _ row: Int, _ changedContact: Contact) {
-        
         contacts[section][row] = changedContact
     }
     
     func showContact(_ section: Int, _ row: Int) -> Contact {
         return contacts[section][row]
+    }
+    
+    func getSectionSize() -> Int {
+//        var size = 0
+//        for contact in contacts {
+//            size += contact.count == 0 ? 0 : 1
+//        }
+//        return size
+        return contacts.count
+    }
+    
+    func getRowSize(_ section: Int) -> Int {
+        return contacts[section].count
     }
 }
