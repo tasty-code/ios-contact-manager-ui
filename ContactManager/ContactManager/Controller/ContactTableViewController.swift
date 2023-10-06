c0e1b9f539f6a87725e9228f25a18c3f7c97be74
COMDORI <02145s1@gmail.com>
2023-10-06 16:37:18 +0900

    style 줄정리

--- ContactManager/ContactManager/Controller/ContactTableViewController.swift
+++ ContactManager/ContactManager/Controller/ContactTableViewController.swift
@@ -8,17 +8,14 @@
 import UIKit
 
 class ContactTableViewController: UITableViewController {
-
     private let contactManager = ContactManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadJsonData()
-        
     }
 
     // MARK: - Table view data source
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let count = contactManager.getContactList().count
         return count
@@ -26,11 +23,14 @@ class ContactTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
+        
         let name = contactManager.getContactList()[indexPath.row].name ?? ""
         let age = contactManager.getContactList()[indexPath.row].age ?? 0
         let phoneNumber = contactManager.getContactList()[indexPath.row].phoneNum ?? ""
+        
         cell.textLabel!.text = "\(name) (\(age))"
         cell.detailTextLabel!.text = phoneNumber
+        
         return cell
     }
 }
@@ -46,7 +46,7 @@ extension ContactTableViewController {
             let root = try JSONDecoder().decode(Root.self, from: data)
             contactManager.setContactList(list: root.data)
         } catch {
-            print("error")
+            print(error)
         }
     }
 }
--- ContactManager/ContactManager/Model/ContactManager.swift
+++ ContactManager/ContactManager/Model/ContactManager.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import UIKit
 
 final class ContactManager {
     private var contactList: [ContactInfo] = []
