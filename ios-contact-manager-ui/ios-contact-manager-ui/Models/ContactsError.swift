0bae044f6d569184017639032ae6acf4c98db80b
JinUng41 <ingjwjw@naver.com>
2023-10-10 13:22:07 +0900

    feat: 이름에 대한 에러 케이스를 정의

--- ios-contact-manager-ui/ios-contact-manager-ui/Models/ContactsError.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Models/ContactsError.swift
@@ -11,15 +11,18 @@ enum ContactsError: Error, CustomStringConvertible {
     case duplicateNumberFound
     case invalidPhoneNumber
     case invalidAge
+    case invalidName
     
     var description: String {
         switch self {
         case .duplicateNumberFound:
             return "이미 존재하는 번호입니다. \n 다시 입력해 주세요!"
         case .invalidPhoneNumber:
-            return "번호 형식이 맞지 않습니다. \n 다시 입력해 주세요!"
+            return "입력한 연락처\n정보가 잘못되었습니다"
         case .invalidAge:
-            return "나이 형식이 맞지 않습니다. \n 다시 입력해 주세요!"
+            return "입력한 나이정보가 잘못되었습니다"
+        case .invalidName:
+            return "입력한 이름 정보가 잘못되었습니다"
         }
     }
 }
