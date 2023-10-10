ff0388470c1f3f65e701236939ed2e3bcfe3fc81
DongJun96 <djlee1201@naver.com>
2023-10-10 17:00:53 +0900

    feat: cancel눌렀을 때 alert기능 추가

--- ios-contact-manager-ui/ios-contact-manager-ui/Controllers/NewContactViewController.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Controllers/NewContactViewController.swift
@@ -18,8 +18,20 @@ class NewContactViewController: UIViewController {
     }
     
     @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "정말로 취소하시겠습니까?", message: nil, preferredStyle: .alert)
+        
+        let yesAction = UIAlertAction(title: "예", style: .destructive) { _ in
+            self.dismiss(animated: true)
+        }
+        let noAction = UIAlertAction(title: "아니오", style: .default)
+        
+        alert.addAction(noAction)
+        alert.addAction(yesAction)
+        
+        self.present(alert, animated: true)
     }
     
     @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        
     }
 }
