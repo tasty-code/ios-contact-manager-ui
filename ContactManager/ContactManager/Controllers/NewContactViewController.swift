f67bba1ef6d433120eed63317c0edd6c9e1bb8f6
imSeongHyeon <jfknfj920@naver.com>
2023-10-11 14:14:04 +0900

    feat: 새 연락처 추가 화면의 cancel 버튼의 alert 구현

--- ContactManager/ContactManager/Controllers/NewContactViewController.swift
+++ ContactManager/ContactManager/Controllers/NewContactViewController.swift
@@ -12,12 +12,19 @@ class NewContactViewController: UIViewController {
     @IBOutlet weak var cancelButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
     @IBAction func touchUpCancelButton(_ sender: UIBarButtonItem) {
-        dismiss(animated: true)
+        let alertController = UIAlertController(title: "정말로 취소하시겠습니까?",
+                          message: nil,
+                          preferredStyle: .alert)
+        let noAlertAction = UIAlertAction(title: "아니오", style: .default, handler: nil)
+        let yesAlertAction = UIAlertAction(title: "예", style: .destructive, handler: { _ in
+            self.dismiss(animated: true)
+        })
+        
+        alertController.addAction(noAlertAction)
+        alertController.addAction(yesAlertAction)
+        present(alertController, animated: true)
     }
-    
-    
 }
