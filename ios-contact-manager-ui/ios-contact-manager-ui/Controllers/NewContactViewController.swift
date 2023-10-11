95655f0f1fe3121a71a16e71f076cb39eaaa2c79
JinUng41 <ingjwjw@naver.com>
2023-10-11 10:58:48 +0900

    feat: 연락처 추가 기능 부분 구현 & Alert 함수 구현
    하이픈을 삽입하는 문제를 해결해야 함.

--- ios-contact-manager-ui/ios-contact-manager-ui/Controllers/NewContactViewController.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Controllers/NewContactViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class NewContactViewController: UIViewController {
+final class NewContactViewController: UIViewController {
 
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var ageTextField: UITextField!
@@ -20,20 +20,38 @@ class NewContactViewController: UIViewController {
     }
     
     @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
-        let alert = UIAlertController(title: "정말로 취소하시겠습니까?", message: nil, preferredStyle: .alert)
-        
-        let yesAction = UIAlertAction(title: "예", style: .destructive) { _ in
-            self.dismiss(animated: true)
-        }
-        let noAction = UIAlertAction(title: "아니오", style: .default)
-        
-        alert.addAction(noAction)
-        alert.addAction(yesAction)
-        
-        self.present(alert, animated: true)
+        showAlert(with: "정말로 취소하시겠습니까?")
     }
     
     @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
-        
+        guard let contactManager = contactManager,
+              let name = nameTextField.text,
+              let age = ageTextField.text,
+              let phoneNumber = phoneNumberTextField.text else { return }
+        do {
+            contactManager.addContact(try Contact(name: name, age: age, phoneNumber: phoneNumber))
+            self.dismiss(animated: true)
+        } catch {
+            showAlert(with: String(describing: error))
+        }
+    }
+}
+
+extension NewContactViewController {
+    private func showAlert(with title: String) {
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        if title == "정말로 취소하시겠습니까?" {
+            let noAction = UIAlertAction(title: "아니오", style: .default)
+            alert.addAction(noAction)
+            
+            let yesAction = UIAlertAction(title: "예", style: .destructive) { _ in
+                self.dismiss(animated: true)
+            }
+            alert.addAction(yesAction)
+        } else {
+            let yesAction = UIAlertAction(title: "확인", style: .default)
+            alert.addAction(yesAction)
+        }
+        self.present(alert, animated: true)
     }
 }
