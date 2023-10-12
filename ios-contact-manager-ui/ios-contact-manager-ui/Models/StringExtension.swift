33f207d9c93fb76cfe0fcb06f98c74bac2db7cbb
DongJun96 <djlee1201@naver.com>
2023-10-12 11:19:48 +0900

    refactor: 전화번호 구분자를 외부에서 전달 받도록 수정

--- ios-contact-manager-ui/ios-contact-manager-ui/Models/StringExtension.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Models/StringExtension.swift
@@ -8,23 +8,22 @@
 import Foundation
 
 extension String {
-    func formattingPhoneNumber() -> String {
-        let hyphen: Character = "-"
+    func formattingPhoneNumber(divider: Character) -> String {
         let count = self.count
         var temp = self
         
         switch count {
         case 3...5:
-            temp.insert(hyphen, at: temp.index(temp.startIndex, offsetBy: 2))
+            temp.insert(divider, at: temp.index(temp.startIndex, offsetBy: 2))
         case 6...9:
-            temp.insert(hyphen, at: temp.index(temp.startIndex, offsetBy: 2))
-            temp.insert(hyphen, at: temp.index(temp.endIndex, offsetBy: 5 - count))
+            temp.insert(divider, at: temp.index(temp.startIndex, offsetBy: 2))
+            temp.insert(divider, at: temp.index(temp.endIndex, offsetBy: 5 - count))
         case 10:
-            temp.insert(hyphen, at: temp.index(temp.startIndex, offsetBy: 3))
-            temp.insert(hyphen, at: temp.index(temp.endIndex, offsetBy: 6 - count))
+            temp.insert(divider, at: temp.index(temp.startIndex, offsetBy: 3))
+            temp.insert(divider, at: temp.index(temp.endIndex, offsetBy: 6 - count))
         case 11:
-            temp.insert(hyphen, at: temp.index(temp.startIndex, offsetBy: 3))
-            temp.insert(hyphen, at: temp.index(temp.endIndex, offsetBy: -4))
+            temp.insert(divider, at: temp.index(temp.startIndex, offsetBy: 3))
+            temp.insert(divider, at: temp.index(temp.endIndex, offsetBy: -4))
         default:
             break
         }
