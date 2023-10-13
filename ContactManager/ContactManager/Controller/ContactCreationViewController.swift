e3de660006af249671c363e37623ac95f8710f88
COMDORI <02145s1@gmail.com>
2023-10-13 09:53:14 +0900

    feat 새 연락처 뷰 네비게이션바 추가

--- ContactManager/ContactManager/Controller/ContactCreationViewController.swift
+++ ContactManager/ContactManager/Controller/ContactCreationViewController.swift
@@ -9,16 +9,43 @@ import UIKit
 
 class ContactCreationViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setup()
-    }
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           setup()
+       }
+       
+       func setup() {
+           self.view.backgroundColor = .systemBackground
+           setNavigationBar()
+       }
+       
+       func setNavigationBar() {
+           let navbar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 50))
+           navbar.backgroundColor = .clear
+           navbar.barTintColor = .systemBackground
+           
+           navbar.setBackgroundImage(UIImage(), for: .default)
+           navbar.shadowImage = UIImage()
+           navbar.layer.shadowColor = UIColor.clear.cgColor
+           navbar.isTranslucent = true
+           
+           let navItem = UINavigationItem(title: "새 연락처")
+           
+           let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(didTapCancelButton))
+           let saveButton = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(didTapSaveButton))
+           
+           navItem.leftBarButtonItem = cancelButton
+           navItem.rightBarButtonItem = saveButton
+           
+           navbar.setItems([navItem], animated: false)
+           self.view.addSubview(navbar)
+       }
+       
+       @objc func didTapCancelButton(_ sender: UINavigationItem) {
+           dismiss(animated: true)
+       }
     
-    func setup() {
-        self.view.backgroundColor = .systemBackground
-        view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        view.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-    }
+       @objc func didTapSaveButton(_ sender: UINavigationItem) {
+           print("save tapped")
+       }
 }
