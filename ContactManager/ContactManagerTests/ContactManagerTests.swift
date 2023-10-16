bd660a37c2f789dfc0934b7d91f5a39bae7c25f9
dpwns1234 <52391722+dpwns1234@users.noreply.github.com>
2023-10-16 17:51:15 +0900

    test: 동작하지 않는 테스트 코드 수정
    - given, when, then 형식 기반으로 작성

--- ContactManager/ContactManagerTests/ContactManagerTests.swift
+++ ContactManager/ContactManagerTests/ContactManagerTests.swift
@@ -11,52 +11,54 @@ import XCTest
 final class ContactManagerTests: XCTestCase {
   var manager = ContactManager()
   
-  func test_이름이_비었으면_false () {
-    let result = manager.nameValidate("")
-    XCTAssertFalse(result)
+  func test_이름이_비었으면_false () throws {
+    // given
+    let input = ""
+    
+    // then
+    XCTAssertThrowsError(try manager.addContact(nameText: input, ageText: "22", phoneText: "010-4234-6212")) { error in
+      XCTAssertEqual(error as! ValidationError, ValidationError.nameValidationError)
+    }
   }
   
   func test_나이가_3자리_수_초과하면_false () {
-    let result = manager.ageValidate(1000)
-    XCTAssertFalse(result)
+    // given
+    let input = "1000"
+    
+    // then
+    XCTAssertThrowsError(try manager.addContact(nameText: "yohan", ageText: input, phoneText: "010-4234-6212")) { error in
+      XCTAssertEqual(error as! ValidationError, ValidationError.ageValidationError)
+    }
   }
   
   func test_나이가_음수면_false () {
-    let result = manager.ageValidate(-5)
-    XCTAssertFalse(result)
+    // given
+    let input = "-5"
+    
+    // then
+    XCTAssertThrowsError(try manager.addContact(nameText: "yohan", ageText: input, phoneText: "010-4234-6212")) { error in
+      XCTAssertEqual(error as! ValidationError, ValidationError.ageValidationError)
+    }
   }
   
-  func test_phone_번호가_9자리_미만이면_false () {
-    let result = manager.phoneValidate("05-343-2443")
-    XCTAssertFalse(result)
+  func test_phone_번호가_9자리_미만이면_false() {
+    // given
+    let input = "05-3432"
+    
+    // then
+    XCTAssertThrowsError(try manager.addContact(nameText: "yohan", ageText: "22", phoneText: input)) { error in
+      XCTAssertEqual(error as! ValidationError, ValidationError.phoneValidationError)
+    }
   }
   
-  func test_phone_번호가_9자리면_true () {
-    let result = manager.phoneValidate("050-343-2443")
-    XCTAssertTrue(result)
+  func test_phone_번호가_9자리면_true() throws {
+    // given
+    let input = "050-343-2443"
+    
+    // when
+    let result: () = try manager.addContact(nameText: "yohan", ageText: "22", phoneText: input)
+
+    // then
+    XCTAssertNoThrow(result)
   }
-
-//    override func setUpWithError() throws {
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//    }
-//
-//    override func tearDownWithError() throws {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
-//
-//    func testExample() throws {
-//        // This is an example of a functional test case.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//        // Any test you write for XCTest can be annotated as throws and async.
-//        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-//        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-//    }
-//
-//    func testPerformanceExample() throws {
-//        // This is an example of a performance test case.
-//        measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
-
 }
