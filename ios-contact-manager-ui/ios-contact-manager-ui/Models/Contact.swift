ee9419ef60cd3efa7652cce8d36499b91363ce8f
JinUng41 <ingjwjw@naver.com>
2023-10-19 16:10:24 +0900

    feat: Contact 클래스의 속성을 private(set), update 메서드 구현

--- ios-contact-manager-ui/ios-contact-manager-ui/Models/Contact.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Models/Contact.swift
@@ -7,26 +7,22 @@
 
 import Foundation
 
-final class Contact: Codable, Validatable, Identifiable {
-    var name: String
-    var age: String
-    var phoneNumber: String
+typealias ContactInfo = (String, String, String)
+
+final class Contact: Codable, Identifiable {
+    private(set) var name: String
+    private(set) var age: String
+    private(set) var phoneNumber: String
     
-    init(name: String, age: String, phoneNumber: String) throws {
-        self.name = name
-        self.age = age
-        self.phoneNumber = phoneNumber
-        
-        guard verifyName(name) else {
-            throw ContactsError.invalidName
-        }
-        
-        guard verifyAge(age) else {
-            throw ContactsError.invalidAge
-        }
-        
-        guard verifyNumber(phoneNumber) else {
-            throw ContactsError.invalidPhoneNumber
-        }
+    init(info: ContactInfo) {
+        self.name = info.0
+        self.age = info.1
+        self.phoneNumber = info.2
+    }
+    
+    func update(with info: ContactInfo) {
+        self.name = info.0
+        self.age = info.1
+        self.phoneNumber = info.2
     }
 }
