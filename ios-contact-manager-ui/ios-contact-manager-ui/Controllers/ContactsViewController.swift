afcae2c6db919b99d93fbe8593a5af68240460a9
JinUng41 <ingjwjw@naver.com>
2023-10-20 10:53:33 +0900

    refactor: 이름 검색 시 동작하기 위한 조건을 변경
    기존에는 전역변수로 조건을 판별했다면, IBOutlet으로 선언한 searchBar의 텍스트로 조건을 판별하도록 하였음.

--- ios-contact-manager-ui/ios-contact-manager-ui/Controllers/ContactsViewController.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Controllers/ContactsViewController.swift
@@ -15,8 +15,6 @@ final class ContactsViewController: UIViewController {
     private let contactManager = ContactManager()
     private let cellIdentifier = "ContactCell"
     private let newContactVCIdentifier = "NewContactViewController"
-    private var isSearching = false
-    private var searchingName = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,15 +35,19 @@ final class ContactsViewController: UIViewController {
 
 extension ContactsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return isSearching ? contactManager.fetchContactsContains(with: searchingName).count : contactManager.contactsCount
+        if let name = searchBar.text, !name.isEmpty {
+            return contactManager.fetchContactsContains(with: name).count
+        } else {
+            return contactManager.contactsCount
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath) as? ContactCell else { return UITableViewCell() }
         var contact: Contact
         
-        if isSearching {
-            let contacts = contactManager.fetchContactsContains(with: searchingName)
+        if let name = searchBar.text, !name.isEmpty {
+            let contacts = contactManager.fetchContactsContains(with: name)
             contact = contacts[indexPath.row]
         } else {
             contact = contactManager.showContact(index: indexPath.row)
@@ -110,12 +112,6 @@ extension ContactsViewController: ContactsTableViewUpdateDelegate {
 
 extension ContactsViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if searchText.isEmpty {
-            self.isSearching = false
-        } else {
-            self.isSearching = true
-            self.searchingName = searchText
-        }
         self.contactsTableView.reloadData()
     }
 }
