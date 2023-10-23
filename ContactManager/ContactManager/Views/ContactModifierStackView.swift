ac4beb57676b7c69dfe1f1e8dddaf720800709ff
comdori <02145s1@gmail.com>
2023-10-23 16:43:40 +0900

    chore 시스템 에러 형태 변경
    강제로 크래쉬 발생하지 않게 변경

--- ContactManager/ContactManager/Views/ContactModifierStackView.swift
+++ ContactManager/ContactManager/Views/ContactModifierStackView.swift
@@ -50,6 +50,6 @@ final class ContactModifierStackView: UIStackView {
     }
     
     required init(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
 }
