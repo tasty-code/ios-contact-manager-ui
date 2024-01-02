c42094e287846f49e4ef1b8a0ab11fbc8c21492e
MyNB1 <dishdib61@naver.com>
2024-01-02 17:42:06 +0900

    config: 프로젝트파일 생성

---
+++ ContactManager/ContactManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactManager
+//
+//  Created by 둘리 on 2024/01/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
