7c72ea0bd67110aabc00f56462627791eff8e178
suojae3 <suojae3@gmail.com>
2024-01-03 14:38:34 +0900

    fix: 테이블뷰 메서드 수정

--- ios-contact-manager-ui/ios-contact-manager-ui/ViewController/PhoneBookViewController.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/ViewController/PhoneBookViewController.swift
@@ -22,9 +22,6 @@ private extension PhoneBookViewController {
     func setupTableView() {
         tableView.delegate = self
         tableView.dataSource = self
-
-        view.addSubview(tableView)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(TableViewCell.self, forCellReuseIdentifier: TableViewCell.reuseID)
         tableView.tableFooterView = UIView()
     }
@@ -37,6 +34,8 @@ private extension PhoneBookViewController {
     }
     
     func setupConstraints() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
