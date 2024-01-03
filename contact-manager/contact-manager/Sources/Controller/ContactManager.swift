b83cbc036670d2e5a8ee9a24581f910c90949b1a
llamang <yohanlee1230@daum.net>
2024-01-03 14:08:02 +0900

    feat(ContactManager): ContactManagerError 생성 및 적용

--- contact-manager/contact-manager/Sources/Controller/ContactManager.swift
+++ contact-manager/contact-manager/Sources/Controller/ContactManager.swift
@@ -1,20 +1,39 @@
 struct ContactManager {
-    private(set) var contactList: [String: Contact]
+    private(set) var contactDictionary: [String: Contact]
     
-    mutating func addContact(contact: Contact) {
-        contactList[contact.phoneNumber] = contact
+    mutating func addContact(_ contact: Contact) throws {
+        guard contactDictionary[contact.phoneNumber] == nil else {
+            throw ContactManagerError.duplicateContact
+        }
+        contactDictionary[contact.phoneNumber] = contact
     }
     
-    func getContact(phoneNumber: String) -> Contact? {
-        return contactList[phoneNumber]
+    func getContact(phoneNumber: String) throws -> Contact {
+        guard let contact = contactDictionary[phoneNumber] else {
+            throw ContactManagerError.nonExistentContact
+        }
+        return contact
     }
     
-    mutating func updateContact(contact: Contact) {
-        contactList[contact.phoneNumber] = contact
+    mutating func updateContact(_ contact: Contact) throws {
+        try validateContactExists(phoneNumber: contact.phoneNumber)
+        contactDictionary[contact.phoneNumber] = contact
     }
     
-    mutating func deleteContact(phoneNumber: String) {
-        contactList.removeValue(forKey: phoneNumber)
+    mutating func deleteContact(phoneNumber: String) throws {
+        try validateContactExists(phoneNumber: phoneNumber)
+        contactDictionary.removeValue(forKey: phoneNumber)
+    }
+    
+    private func validateContactExists(phoneNumber: String) throws {
+        if contactDictionary[phoneNumber] != nil {
+            throw ContactManagerError.nonExistentContact
+        }
     }
 
+}
+
+enum ContactManagerError: Error {
+    case nonExistentContact
+    case duplicateContact
 }
