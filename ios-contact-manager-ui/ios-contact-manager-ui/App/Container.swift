9cdef0a7737d8235cdc429466d2f7cdc511c5046
suojae3 <suojae3@gmail.com>
2024-01-03 11:38:12 +0900

    fix: fatalError 프린트 문구 수정

--- ios-contact-manager-ui/ios-contact-manager-ui/App/Container.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/App/Container.swift
@@ -17,7 +17,7 @@ final class Container {
     func resolve<Service>(_ type: Service.Type) -> Service {
         let key = String(describing: Service.self)
         guard let service = services[key] as? Service else {
-            fatalError("의존성 바인딩되어있지 않음!")
+            fatalError("주입할 객체가 service 딕셔너리에 바인딩되어있지 않음!")
         }
         return service
     }
