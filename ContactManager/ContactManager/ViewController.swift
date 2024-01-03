5664c4a8d69c73ec1d3bd64e9470c0fe9cabad03
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2024-01-03 16:30:18 +0900

    feat: 초기프로젝트셋업

---
+++ ContactManager/ContactManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactManager
+//
+//  Created by 유니 & L on 1/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
