0a4afbe92c5f960df5aefbc32aa65ba101a7b596
NERO <mxzxro@gmail.com>
2024-01-03 15:18:18 +0900

    config: 프로젝트 생성

---
+++ ios-contact-manager-ui/ios-contact-manager-ui/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-contact-manager-ui
+//
+//  Created by NERO on 1/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
