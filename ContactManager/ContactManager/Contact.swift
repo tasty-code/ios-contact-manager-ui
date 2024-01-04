9ae4c8e29cd51bf5e89dc871cdad72fcb6c1774b
wlsdud-dev <hjymail1025@gmail.com>
2024-01-04 11:05:08 +0900

    feat: 연락처 객체 생성

---
+++ ContactManager/ContactManager/Contact.swift
@@ -0,0 +1,21 @@
+//
+//  Contact.swift
+//  ContactManager
+//
+//  Created by Jin-Mac on 1/4/24.
+//
+
+import Foundation
+
+struct Contact {
+    
+    var name: String
+    var age: Int
+    var phoneNumber: String
+    
+    init(name: String, age: Int, phoneNumber: String) {
+        self.name = name
+        self.age = age
+        self.phoneNumber = phoneNumber
+    }
+}
