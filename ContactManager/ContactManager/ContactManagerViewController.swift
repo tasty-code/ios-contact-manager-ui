256695f133ad2ab89c8127fbdc290979ecb4cef3
MyNB1 <dishdib61@naver.com>
2024-01-04 20:44:01 +0900

    feat: contactTableView 구성 및 접근제어 설정 (#1)

--- ContactManager/ContactManager/ContactManagerViewController.swift
+++ ContactManager/ContactManager/ContactManagerViewController.swift
@@ -8,10 +8,37 @@
 import UIKit
 
 class ContactManagerViewController: UIViewController {
- 
-    @IBOutlet weak var contactTableView: UITableView!
+    
+    @IBOutlet private weak var contactTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUpTableView()
+    }
+    
+    private func setUpTableView() {
+        contactTableView.delegate = self
+        contactTableView.dataSource = self
+    }
+}
+
+extension ContactManagerViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+}
+
+extension ContactManagerViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ContactInfoModel.sampleData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ContactManagerTableViewCell.identifier, for: indexPath) as? ContactManagerTableViewCell else { return UITableViewCell() }
+        
+        cell.setUpData(data: ContactInfoModel.sampleData[indexPath.row])
+        
+        return cell
     }
 }
