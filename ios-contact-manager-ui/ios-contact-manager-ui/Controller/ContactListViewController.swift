eb494fb137c8013c78c0ca4e13434a650c224f92
jus1234 <osjamg1996@gmail.com>
2024-01-04 14:00:31 +0900

    feat: ContactListViewController UITableViewDataSource 추가

--- ios-contact-manager-ui/ios-contact-manager-ui/Controller/ContactListViewController.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Controller/ContactListViewController.swift
@@ -7,12 +7,16 @@
 
 import UIKit
 
-class ContactListViewController: UIViewController {
+final class ContactListViewController: UIViewController {
 
     private let tableView: UITableView
+    private var contactList: ContactList
+    private var contactArray: Array<Contact>
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         tableView = UITableView()
+        contactList = ContactList()
+        contactArray = contactList.showContactList()
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
     
@@ -22,11 +26,31 @@ class ContactListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
+        setupTableViewConstraints()
+    }
+}
+
+extension ContactListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {        return contactArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath) as! ContactTableViewCell
+        let contact = contactArray[indexPath.row]
+        
+        cell.nameLabel.text = "\(contact.name)(\(contact.age))"
+        cell.phoneNumberLabel.text = contact.phoneNumber
+        cell.accessoryType = .disclosureIndicator
+        
+        return cell
     }
 }
 
 extension ContactListViewController {
     private func setupTableView() {
+        tableView.dataSource = self
         tableView.rowHeight = 50
         tableView.register(ContactTableViewCell.self, forCellReuseIdentifier: "ContactCell")
     }
@@ -35,9 +59,10 @@ extension ContactListViewController {
         view.addSubview(tableView)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
         ])
     }
 }
