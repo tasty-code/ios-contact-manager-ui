612d0745a1a59a39c9812b3742840bf578401cd2
EenSung Kim <eensungkim@gmail.com>
2024-01-04 10:29:57 +0900

    design: ContactTableViewCell 스택뷰 안에 레이블 추가

--- ios-contact-manager-ui/ios-contact-manager-ui/View/ContactTableViewCell.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/View/ContactTableViewCell.swift
@@ -11,16 +11,29 @@ final class ContactTableViewCell: UITableViewCell {
     
     let nameLabel: UILabel = {
         let label: UILabel = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17)
         return label
     }()
     
     let phoneNumberLabel: UILabel = {
         let label: UILabel = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17)
         return label
     }()
     
     let stackView: UIStackView = {
         let sv: UIStackView = UIStackView()
+        sv.axis = .vertical
+        sv.distribution = .fill
+        sv.alignment = .fill
         return sv
     }()
+}
+
+extension ContactTableViewCell {
+    private func setupStackView() {
+        self.contentView.addSubview(stackView)
+        stackView.addArrangedSubview(nameLabel)
+        stackView.addArrangedSubview(phoneNumberLabel)
+    }
 }
