8f83e68f60d3d05eb0a6838d820bd1b4db8751a4
MyNB1 <dishdib61@naver.com>
2024-01-04 20:25:40 +0900

    feat: ContactManagerViewController 추가 및 TableView UI 구성 (#1)

---
+++ ContactManager/ContactManager/ContactManagerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ContactManagerViewController.swift
+//  ContactManager
+//
+//  Created by 둘리 on 2024/01/03.
+//
+
+import UIKit
+
+class ContactManagerViewController: UIViewController {
+ 
+    @IBOutlet weak var contactTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
