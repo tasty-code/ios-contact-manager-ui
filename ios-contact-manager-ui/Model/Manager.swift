03cacc1be58514a3d3d858c31c64d1669df001ea
hemil0102 <hemil0102@gmail.com>
2024-01-04 17:25:58 +0900

    feat: 연락처 모델 구현 (#1)

---
+++ ios-contact-manager-ui/Model/Manager.swift
@@ -0,0 +1,14 @@
+//
+//  Manager.swift
+//  ios-contact-manager-ui
+//
+//  Created by Harry Ho on 1/4/24.
+//
+
+import Foundation
+
+struct Manager {
+    var name: String
+    var age: Int
+    var number: Int
+}
