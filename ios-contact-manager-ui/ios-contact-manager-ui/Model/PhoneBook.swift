67e92b3338d21567e5acba9aebb3e7b931dea6c9
suojae3 <suojae3@gmail.com>
2024-01-04 17:38:43 +0900

    feat: 더미데이터 추가 및 타입 배열로 변경

--- ios-contact-manager-ui/ios-contact-manager-ui/Model/PhoneBook.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Model/PhoneBook.swift
@@ -4,10 +4,19 @@ import Foundation
 // MARK: - PhoneBooks Init & Deinit
 final class PhoneBook {
     
-    lazy var categorizedBook: [Tag: User] = [.friend: dummyUser1]
+    lazy var categorizedBook = [Tag: [User]]()
     
-    let dummyUser1 = User(userID: UUID(), name: "김철수", phoneNumber: "010-1111-2222", age: 20)
-        
+    let dummyUser1 = User(userID: UUID(), name: "김철수", phoneNumber: "010-1111-2223", age: 10)
+    let dummyUser2 = User(userID: UUID(), name: "강철수", phoneNumber: "010-1111-2224", age: 20)
+    let dummyUser3 = User(userID: UUID(), name: "나철수", phoneNumber: "010-1111-2225", age: 30)
+    let dummyUser4 = User(userID: UUID(), name: "미철수", phoneNumber: "010-1111-2226", age: 40)
+    let dummyUser5 = User(userID: UUID(), name: "쿠철수", phoneNumber: "010-1111-2227", age: 50)
+    let dummyUser6 = User(userID: UUID(), name: "기철수", phoneNumber: "010-1111-2228", age: 60)
+    let dummyUser7 = User(userID: UUID(), name: "바철수", phoneNumber: "010-1111-2229", age: 70)
+    let dummyUser8 = User(userID: UUID(), name: "비철수", phoneNumber: "010-1111-2221", age: 80)
+    let dummyUser9 = User(userID: UUID(), name: "강철수", phoneNumber: "010-1111-2220", age: 90)
+    let dummyUser10 = User(userID: UUID(), name: "자철수", phoneNumber: "010-1111-2202", age: 100)
+
     deinit { print("PhoneBook has been deinit!!")}
     
 }
