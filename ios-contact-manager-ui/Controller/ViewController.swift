1908277d7a8424827be290d9130c5f5556a9f8be
Hoi.Kwon <teaho177@gmail.com>
2024-01-04 18:18:35 +0900

    feat: 테이블뷰 프로토콜 채택 및 준수 (#1)

--- ios-contact-manager-ui/Controller/ViewController.swift
+++ ios-contact-manager-ui/Controller/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var contactTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
-
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
 }
 
