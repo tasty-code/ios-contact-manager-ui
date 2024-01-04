52aac1ebf8051bde922bebe619258998fec75c2d
EenSung Kim <eensungkim@gmail.com>
2024-01-04 10:22:50 +0900

    feat: ContactTableViewCell 생성

---
+++ ios-contact-manager-ui/ios-contact-manager-ui/View/ContactTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  ContactTableViewCell.swift
+//  ios-contact-manager-ui
+//
+//  Created by Kim EenSung on 1/4/24.
+//
+
+import UIKit
+
+final class ContactTableViewCell: UITableViewCell {
+    
+    let nameLabel: UILabel = {
+        let label: UILabel = UILabel()
+        return label
+    }()
+    
+    let phoneNumberLabel: UILabel = {
+        let label: UILabel = UILabel()
+        return label
+    }()
+    
+}
