5d63389e71235f2400504afb9ac8e3a2c6226b40
kimdj1102 <djkim1102@naver.com>
2024-01-05 14:59:52 +0900

    refactor: 띄어쓰기, 오타 수정

--- ios-contact-manager-ui/ios-contact-manager-ui/Controller/ViewController.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Controller/ViewController.swift
@@ -66,9 +66,7 @@ extension ViewController: UITableViewDataSource {
 }
 
 extension ViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
-
-
