eb9ba35fbb0f57eef46cda097793313569606a37
kimdj1102 <djkim1102@naver.com>
2024-01-05 13:24:38 +0900

    feat: tableView layout 코드 추가

---
+++ ios-contact-manager-ui/ios-contact-manager-ui/Controller/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ios-contact-manager-ui
+//
+//  Created by 미르, 루피 on 1/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let table = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        layout()
+    }채ㅜ
+    
+    func layout() {
+        view.addSubview(table)
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        table.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        table.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        table.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+    }
+}
+
+
+
