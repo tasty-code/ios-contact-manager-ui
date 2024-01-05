fb18beef5d835762de20c444e5f03536e2dc80b4
eunjuchoi <eunjuzzoo@naver.com>
2024-01-05 11:33:40 +0900

    feat: UIViewController+ 파일 추가 (#1)

---
+++ ContactManager/ContactManager/Sources/Extension/UIViewController+.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController+.swift
+//  ContactManager
+//
+//  Created by EUNJU on 2024/01/05.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    static var className: String {
+        NSStringFromClass(self.classForCoder()).components(separatedBy: ".").last!
+    }
+    
+    var className: String {
+        NSStringFromClass(self.classForCoder).components(separatedBy: ".").last!
+    }
+}
