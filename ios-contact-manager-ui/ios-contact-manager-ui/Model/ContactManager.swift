67b108ad9a170a2b0a73e0c9a4c5eedd47c4ae36
kimdj1102 <djkim1102@naver.com>
2024-01-05 14:29:09 +0900

    feat: 연락처 관리 기능 추가

---
+++ ios-contact-manager-ui/ios-contact-manager-ui/Model/ContactManager.swift
@@ -0,0 +1,28 @@
+//
+//  ContactManager.swift
+//  ios-contact-manager-ui
+//
+//  Created by 미르, 루피 on 1/5/24.
+//
+
+import Foundation
+
+final class ContactManager {
+    var contacts: [Contact] = []
+    
+    func add(contact: Contact) {
+        contacts.append(contact)
+    }
+    
+    func delete(index: IndexPath) {
+        contacts.remove(at: index.row)
+    }
+    
+    func show(index: IndexPath) -> Contact {
+        return contacts[index.row]
+    }
+    
+    func modify(contact: Contact, indexPath: IndexPath) {
+        contacts[indexPath.row] = contact
+    }
+}
