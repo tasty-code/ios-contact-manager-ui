fe25e6579958473460057406ae2755a4cc682947
Effie <effie.thedev@gmail.com>
2024-01-05 14:24:39 +0900

    chore: 프로젝트 생성

---
+++ ContactManager/ContactManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactManager
+//
+//  Created by Effie on 1/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
