9d9404e12410683c06de3672fc582b7e583358d2
angryeon7 <nayeon7612@gmail.com>
2024-01-05 11:30:28 +0900

    config: 프로젝트 초기 세팅

---
+++ Contact-Manager-iOS/Contact-Manager-iOS/Controllers/ContactsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ContactsViewController.swift
+//  Contact-Manager-iOS
+//
+//  Created by nayeon  on 2024/01/05.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+    
+}
