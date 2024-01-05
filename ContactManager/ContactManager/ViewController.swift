95170cf776009f38253cd09e8d6d95fc3fbae968
LeeSe0ngYe0n <goblin__@naver.com>
2024-01-05 11:54:32 +0900

    feat: 테이블뷰 생성

--- ContactManager/ContactManager/ViewController.swift
+++ ContactManager/ContactManager/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
