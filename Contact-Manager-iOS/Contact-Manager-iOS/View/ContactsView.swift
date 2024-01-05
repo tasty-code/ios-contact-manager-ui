c0c7ac3c0449049b132c09adecddc273dcf5287c
Andy An <andyanappdev@gmail.com>
2024-01-05 14:28:28 +0900

    feat: 연락처 화면의 UIView 구현

---
+++ Contact-Manager-iOS/Contact-Manager-iOS/View/ContactsView.swift
@@ -0,0 +1,44 @@
+//
+//  ContactsView.swift
+//  Contact-Manager-iOS
+//
+//  Created by Doyoung An on 1/5/24.
+//
+
+import UIKit
+
+class ContactsView: UIView {
+    
+    //MARK: - Property
+    let contactsTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    
+    //MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .white
+        configureContactsTableView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    //MARK: - Method
+    func configureContactsTableView() {
+        self.addSubview(contactsTableView)
+        
+        NSLayoutConstraint.activate([
+            contactsTableView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 0),
+            contactsTableView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: 0),
+            contactsTableView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            contactsTableView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: 0)
+        ])
+    }
+}
