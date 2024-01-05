fcb950f06e3bfa7eb7fb741df091630081ff84ae
angryeon7 <nayeon7612@gmail.com>
2024-01-05 14:07:14 +0900

    feat: 연락처 관리 class 구현

---
+++ Contact-Manager-iOS/Contact-Manager-iOS/Models/ContactManager.swift
@@ -0,0 +1,29 @@
+//
+//  ContactManager.swift
+//  Contact-Manager-iOS
+//
+//  Created by nayeon  on 2024/01/05.
+//
+class ContactManager {
+    
+    // MARK: - Property
+    private var contactList: [Contact] = []
+
+    
+    // MARK: - Method
+    func showUpAllContacts() -> [Contact] {
+        return contactList
+    }
+    
+    func addNewContacts(contact: Contact) {
+        contactList.append(contact)
+    }
+    
+    func deleteContact(at index: Int){
+        contactList.remove(at: index)
+    }
+    
+    func updateContact(at index: Int, with updateContact: Contact) {
+        contactList[index] = updateContact
+    }
+}
