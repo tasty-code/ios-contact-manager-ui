7eb2872335436f5d05332a87871aa41461f27644
angryeon7 <nayeon7612@gmail.com>
2024-01-05 15:48:44 +0900

    feat: 연락처화면의 ViewController 구현

--- Contact-Manager-iOS/Contact-Manager-iOS/Controllers/ContactsViewController.swift
+++ Contact-Manager-iOS/Contact-Manager-iOS/Controllers/ContactsViewController.swift
@@ -9,4 +9,49 @@ import UIKit
 
 class ContactsViewController: UIViewController {
     
+    //MARK: - Property
+    private let contactManager: ContactManager = ContactManager()
+    private let contactsView: ContactsView = ContactsView()
+    
+    
+    //MARK: - Life Cycle
+    override func loadView() {
+        self.view = contactsView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureTableView()
+    }
+    
+    
+    //MARK: - Method
+    private func configureTableView() {
+        contactsView.contactsTableView.dataSource = self
+        contactsView.contactsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "ContactTableViewCell")
+    }
+    
+}
+    
+
+//MARK: - Extension
+extension ContactsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contactManager.showUpAllContacts().count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "ContactTableViewCell")
+        let contact = contactManager.showUpAllContacts()[indexPath.row]
+        
+        cell.textLabel?.text = "\(contact.name ?? "알수없는 사용자")(\(contact.age ?? "0"))"
+        cell.detailTextLabel?.text = contact.contctNumber
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 16)
+        cell.detailTextLabel?.font = UIFont.systemFont(ofSize: 16)
+        cell.accessoryType = .disclosureIndicator
+        
+        return cell
+    }
+    
 }
