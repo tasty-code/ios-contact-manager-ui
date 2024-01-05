eb1505c3f0c9ff9008b49fe63a7edbcf51d139a1
ijs1103 <ajaijs1103@naver.com>
2024-01-05 16:09:09 +0900

    chore(SceneDelegate): self 키워드 생략

--- contact-manager/contact-manager/Sources/Application/SceneDelegate.swift
+++ contact-manager/contact-manager/Sources/Application/SceneDelegate.swift
@@ -7,10 +7,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        self.window = UIWindow(windowScene: windowScene)
+        window = UIWindow(windowScene: windowScene)
         let contactManager = ContactManager(contactDictionary: contactData)
-        self.window?.rootViewController = ViewController(contactManager: contactManager)
-        self.window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController(contactManager: contactManager)
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
