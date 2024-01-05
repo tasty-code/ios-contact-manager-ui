4ec31b47a287aa9e4140aad5bd9714521c088a00
kimdj1102 <djkim1102@naver.com>
2024-01-05 13:30:58 +0900

    feat: gitignore파일 추가

--- ios-contact-manager-ui/ios-contact-manager-ui/Controller/ViewController.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Controller/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         layout()
-    }채ㅜ
+    }
     
     func layout() {
         view.addSubview(table)
