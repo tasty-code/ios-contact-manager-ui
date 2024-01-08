faf0b8368de619178daaa8bbb6f0b491e3aaefb1
Effie <effie.thedev@gmail.com>
2024-01-08 16:52:52 +0900

    feat: Contact 구조체 추가
    Co-authored-by: Hong <ujhong7@gmail.com>

---
+++ ContactManager/ContactManager/Model/Contact.swift
@@ -0,0 +1,14 @@
+//
+//  Contact.swift
+//  ContactManager
+//
+//  Created by Effie on 1/6/24.
+//
+
+struct Contact {
+    let name: String
+    
+    let phoneNumber: String
+    
+    let age: Int
+}
