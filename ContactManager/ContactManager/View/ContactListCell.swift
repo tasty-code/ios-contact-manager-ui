b8c60c138658158a14b8305f9ee8b1231e528dbd
Effie <effie.thedev@gmail.com>
2024-01-08 16:56:53 +0900

    feat: ContactListCell view 추가 및 레이아웃 로직 구현
    Co-authored-by: Hong <ujhong7@gmail.com>

--- ContactManager/ContactManager/View/ContactListCell.swift
+++ ContactManager/ContactManager/View/ContactListCell.swift
@@ -8,16 +8,52 @@
 import UIKit
 
 final class ContactListCell: UITableViewCell {
+    private let infoLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.font = .preferredFont(forTextStyle: .body)
+        return label
+    }()
+    
+    private let phoneNumberLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.font = .preferredFont(forTextStyle: .body)
+        return label
+    }()
+    
+    private lazy var labelStack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [infoLabel, phoneNumberLabel])
+        stack.axis = .vertical
+        stack.spacing = 2
+        stack.distribution = .fill
+        stack.translatesAutoresizingMaskIntoConstraints = false
+//        stack.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        return stack
+    }()
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        setLayout()
     }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setLayout()
+    }
+    
+    private func setLayout() {
+        self.contentView.addSubview(self.labelStack)
+        NSLayoutConstraint.activate([
+            self.labelStack.leadingAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.leadingAnchor),
+            self.labelStack.trailingAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.trailingAnchor),
+            self.labelStack.topAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.topAnchor),
+            self.labelStack.bottomAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.bottomAnchor),
+        ])
     }
     
     func update(with newContact: Contact) {
-        
+        let info = "\(newContact.name)(\(newContact.age))"
+        self.infoLabel.text = info
+        self.phoneNumberLabel.text = newContact.phoneNumber
     }
 }
 
