4aa35863c220d17c6963f8380d57ec15e3d77096
Effie <effie.thedev@gmail.com>
2024-01-08 16:54:31 +0900

    feat: ContactListView 및 Cell 타입 추가
    Co-authored-by: Hong <ujhong7@gmail.com>

---
+++ ContactManager/ContactManager/View/ContactListCell.swift
@@ -0,0 +1,18 @@
+//
+//  ContactListCell.swift
+//  ContactManager
+//
+//  Created by Effie on 1/6/24.
+//
+
+import UIKit
+
+final class ContactListCell: UITableViewCell {
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+}
---
+++ ContactManager/ContactManager/View/ContactListView.swift
@@ -0,0 +1,21 @@
+//
+//  ContactListView.swift
+//  ContactManager
+//
+//  Created by Effie on 1/6/24.
+//
+
+import UIKit
+
+final class ContactListView: UITableView {
+    typealias ContactCell = ContactListCell
+    
+    convenience init() {
+        self.init(frame: .zero, style: .plain)
+        setCollection()
+    }
+    
+    private func setCollection() {
+        register(ContactCell.self, forCellReuseIdentifier: "\(ContactCell.self)")
+    }
+}
