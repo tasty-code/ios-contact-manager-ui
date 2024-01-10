7c908969cca7aa000f3144c7f54b4db83295399d
hemil0102 <hemil0102@gmail.com>
2024-01-10 17:18:20 +0900

    design: 연락처 추가 화면 네비게이션 컨틀롤러 생성 (#2)

---
+++ ios-contact-manager-ui/Controller/AddContactViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AddContanctViewController.swift
+//  ios-contact-manager-ui
+//
+//  Created by Harry Ho on 1/10/24.
+//
+
+import UIKit
+
+class AddContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
