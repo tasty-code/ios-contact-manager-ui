b2765026c11efe17627ee6445e421894d2802bb2
llamang <yohanlee1230@daum.net>
2024-01-11 14:06:53 +0900

    feat(InputValidator): InputValidator enum 생성

---
+++ contact-manager/contact-manager/Sources/Util/InputValidator.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+enum InputValidator {
+    static func isValidName(_ name: String) -> Bool {
+        return RegexHelper.matchesRegex(name, regex: RegexPattern.noWhitespace)
+    }
+    
+    static func isValidAge(_ age: String) -> Bool {
+        return RegexHelper.matchesRegex(age, regex: RegexPattern.onlyNumbers)
+    }
+    
+    static func isValidPhoneNumber(_ phoneNumber: String) -> Bool {
+        return RegexHelper.matchesRegex(phoneNumber, regex: RegexPattern.phoneNumber)
+    }
+}
