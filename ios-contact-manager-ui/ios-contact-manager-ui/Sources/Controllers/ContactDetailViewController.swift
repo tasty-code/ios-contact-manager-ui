0b006b9a7adf63b61295c3efbbaf7ed1977ef40b
kimdj1102 <djkim1102@naver.com>
2024-01-11 15:39:21 +0900

    feat: contactDeatilView navigation bar 생성 및 레이아웃 설정

--- ios-contact-manager-ui/ios-contact-manager-ui/Sources/Controllers/ContactDetailViewController.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Sources/Controllers/ContactDetailViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 class ContactDetailViewController: UIViewController {
 
     private let detailView = DetailView()
+    var contact: Contact?
+    
+    lazy var navigationBar: UINavigationBar = {
+        let nvBar = UINavigationBar()
+        nvBar.translatesAutoresizingMaskIntoConstraints = false
+        return nvBar
+    }()
     
     override func loadView() {
         view = detailView
@@ -17,8 +24,35 @@ class ContactDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupnvBar()
+        detailView.contact = contact
 
     }
 
-
+    func setupnvBar() {
+        self.view.backgroundColor = .white
+        
+        self.view.addSubview(navigationBar)
+        let safeArea = self.view.safeAreaLayoutGuide
+        
+        navigationBar.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        navigationBar.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
+        navigationBar.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor).isActive = true
+        
+        let leftButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelButtonTapped))
+        let rtButton = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(setupButtonAction))
+        
+        let navItem = UINavigationItem(title: "새연락처")
+        navItem.leftBarButtonItem = leftButton
+        navItem.rightBarButtonItem = rtButton
+        navigationBar.setItems([navItem], animated: true)
+    }
+    
+    @objc func cancelButtonTapped() {
+        
+    }
+    
+    @objc func setupButtonAction() {
+        
+    }
 }
