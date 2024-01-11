09aef706ca972650aeed544c343c2589a039f664
eensungkim <eensungkim@gmail.com>
2024-01-11 17:21:31 +0900

    chore: 띄어쓰기

--- ios-contact-manager-ui/ios-contact-manager-ui/Model/Contacts.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Model/Contacts.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Contacts: ContactsManageable, ContactsApproachable{
+struct Contacts: ContactsManageable, ContactsApproachable {
     
     private var contactsRepository: Dictionary<Int, Contact>
     
