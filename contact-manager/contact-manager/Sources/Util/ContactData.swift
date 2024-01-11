d854087fe0d853fa53a101d8b650bf22c0d876d5
llamang <yohanlee1230@daum.net>
2024-01-11 13:58:50 +0900

    refactor(Contact): age 프로퍼티 타입을 Int에서 String으로 변경

--- contact-manager/contact-manager/Sources/Util/ContactData.swift
+++
@@ -1,7 +0,0 @@
-let contactData = [
-    "010-1234-5678": Contact(phoneNumber: "010-1234-5678", name: "minsu", age: 30),
-    "010-2234-5678": Contact(phoneNumber: "010-2234-5678", name: "james", age: 21),
-    "010-3234-5678": Contact(phoneNumber: "010-3234-5678", name: "scott", age: 32),
-    "010-4234-5678": Contact(phoneNumber: "010-4234-5678", name: "paul", age: 40),
-    "010-5234-5678": Contact(phoneNumber: "010-5234-5678", name: "pepp", age: 47)
-]
