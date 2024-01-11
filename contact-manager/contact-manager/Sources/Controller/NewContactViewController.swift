05695c824a42d5727a1b8debf287f6da6b283346
ijs1103 <155879048+navijs1013@users.noreply.github.com>
2024-01-11 16:25:10 +0900

    feat(NewContactViewController): NewContactViewController 생성

---
+++ contact-manager/contact-manager/Sources/Controller/NewContactViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+final class NewContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        view.backgroundColor = .white
+        setupNavigationBar()
+        setupInputForm()
+    }
+}
+
+extension NewContactViewController {
+    private func setupNavigationBar() {
+        
+    }
+    
+    private func setupInputForm() {
+        
+    }
+}
