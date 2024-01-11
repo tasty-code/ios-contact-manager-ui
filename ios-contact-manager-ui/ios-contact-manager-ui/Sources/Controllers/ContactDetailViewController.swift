71cac1125ce5a0ffdf55d5c75de9caeea03cb232
mireu <148876644+mireu930@users.noreply.github.com>
2024-01-11 17:29:40 +0900

    refactor: String 확장파일추가

--- ios-contact-manager-ui/ios-contact-manager-ui/Sources/Controllers/ContactDetailViewController.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Sources/Controllers/ContactDetailViewController.swift
@@ -88,35 +88,3 @@ extension ContactDetailViewController {
         detailView.phoneNumberTextField.text = text.formmater
     }
 }
-
-
-extension String {
-    var removeBlank: String {
-        let blanck = self
-        let name = blanck.split(separator: " ").reduce("") { $0 + $1 }
-        
-        return name
-    }
-    
-    public var formmater: String {
-        var stringWithhypen: String = self
-        
-        switch stringWithhypen.count {
-        case 3...5:
-            stringWithhypen.insert("-", at: stringWithhypen.index(stringWithhypen.startIndex, offsetBy: 2))
-        case 6...9:
-            stringWithhypen.insert("-", at: stringWithhypen.index(stringWithhypen.startIndex, offsetBy: 2))
-            stringWithhypen.insert("-", at: stringWithhypen.index(stringWithhypen.endIndex, offsetBy: 5 - count))
-        case 10:
-            stringWithhypen.insert("-", at: stringWithhypen.index(stringWithhypen.startIndex, offsetBy: 3))
-            stringWithhypen.insert("-", at: stringWithhypen.index(stringWithhypen.endIndex, offsetBy: -4))
-        case 11:
-            stringWithhypen.insert("-", at: stringWithhypen.index(stringWithhypen.startIndex, offsetBy: 3))
-            stringWithhypen.insert("-", at: stringWithhypen.index(stringWithhypen.endIndex, offsetBy: -4))
-        default:
-            break
-        }
-        
-        return stringWithhypen
-    }
-}
