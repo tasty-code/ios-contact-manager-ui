d40744d0cd398f3e7b5cc328a787f7b21ed77fed
eensungkim <eensungkim@gmail.com>
2024-01-12 19:12:47 +0900

    feat: 전화번호 입력 시 하이픈 자동입력 기능 구현

--- ios-contact-manager-ui/ios-contact-manager-ui/Controller/ContactsAdditionModalViewController.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Controller/ContactsAdditionModalViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class ContactsAdditionModalViewController: UIViewController {
+final class ContactsAdditionModalViewController: UIViewController, UITextFieldDelegate {
     weak var delegate: ContactsManageable?
     private var regexByTextField: Dictionary<UITextField, String>
     private var invalidationByTextField: Dictionary<UITextField, InvalidationInput>
@@ -21,6 +21,8 @@ final class ContactsAdditionModalViewController: UIViewController {
         sortedTextField = []
         contactsAdditionModalView = ContactsAddtionModalView()
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        contactsAdditionModalView.phoneNumberTextField.delegate = self
         setSortedTextField()
         setRegexByTextField()
         setInvalidationByTextField()
@@ -33,6 +35,87 @@ final class ContactsAdditionModalViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view = contactsAdditionModalView
+    }
+}
+
+extension ContactsAdditionModalViewController {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text else {
+            return false
+        }
+
+        textField.text = string == "" ? deleteKeyTapped(text: text, range: range) : numberKeyTapped(text: text, range: range)
+        return true
+    }
+    
+    private func numberKeyTapped(text: String, range: NSRange) -> String {
+        var resultText = text
+        
+        switch text.starts(with: "0") {
+        case true:
+            switch range.location {
+            case 3, 7:
+                resultText += "-"
+                return resultText
+            case 12:
+                return exchange(string: text, of: 8, isDelete: false)
+            default:
+                return text
+            }
+        case false:
+            switch range.location {
+            case 2, 6:
+                resultText += "-"
+                return resultText
+            case 11:
+                return exchange(string: text, of: 7, isDelete: false)
+            default:
+                return text
+            }
+        }
+    }
+    
+    private func deleteKeyTapped(text: String, range: NSRange) -> String {
+        var resultText = text
+        
+        switch text.starts(with: "0") {
+        case true:
+            switch range.location {
+            case 4, 8:
+                resultText.popLast()
+                return resultText
+            case 12:
+                return exchange(string: text, of: 7, isDelete: true)
+            default:
+                return text
+            }
+        case false:
+            switch range.location {
+            case 3, 7:
+                resultText.popLast()
+                return resultText
+            case 11:
+                return exchange(string: text, of: 6, isDelete: true)
+            default:
+                return text
+            }
+        }
+    }
+    
+    private func exchange(string: String, of: Int, isDelete: Bool) -> String {
+        var textArray: Array<Character> = Array(string)
+        let characterToBeExchanged: Character = textArray[of]
+        textArray[of] = "-"
+        
+        switch isDelete {
+        case true:
+            textArray[of + 1] = characterToBeExchanged
+            textArray.popLast()
+        case false:
+            textArray[of - 1] = characterToBeExchanged
+        }
+        
+        return String(textArray)
     }
 }
 
