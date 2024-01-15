929f1860a880e1fb354be6af465821d8c66fec11
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2024-01-15 19:02:42 +0900

    feat: UITextFieldDelegate 채택 및 하이픈 자동완성기능 구현

--- ContactManager/ContactManager/ViewController/NewContactViewController.swift
+++ ContactManager/ContactManager/ViewController/NewContactViewController.swift
@@ -27,12 +27,7 @@ class NewContactViewController: UIViewController, CustomAlert {
     override func viewDidLoad() {
         super.viewDidLoad()
         changeKeyboardType()
-    }
-    
-    func changeKeyboardType() {
-        nameTextField.keyboardType = .default
-        ageTextField.keyboardType = .numberPad
-        phoneNumberTextField.keyboardType = .numberPad
+        phoneNumberTextField.delegate = self
     }
     
     @IBAction func dismissButtonTapped(_ sender: UIButton) {
@@ -58,5 +53,44 @@ class NewContactViewController: UIViewController, CustomAlert {
         showAlert(message: "입력한 정보가 맞는지 확인하세요.", actions: [cancel, ok])
     }
 }
+extension NewContactViewController {
+    func changeKeyboardType() {
+        nameTextField.keyboardType = .default
+        ageTextField.keyboardType = .numberPad
+        phoneNumberTextField.keyboardType = .numberPad
+    }
+}
 
-
+extension NewContactViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        guard var text = textField.text else { return false }
+        
+        switch (text.hasPrefix("02"), text.count, string.isEmpty) {
+                case (true, 2, false), (true, 6, false):
+                    text += "-"
+                case (false, 3, false), (false, 7, false):
+                    text += "-"
+                case (true, 4, true), (true, 8,true):
+                    if string.isEmpty {
+                        text.removeLast()
+                    }
+                case (false, 4,true), (false, 8,true):
+                    if string.isEmpty {
+                        text.removeLast()
+                    }
+                case (true, 11, false), (false, 12, false):
+                    if let lastHyphenIndex = text.lastIndex(of: "-") {
+                        text.remove(at: lastHyphenIndex)
+                        let indexToAddHyphen = text.index(text.endIndex, offsetBy: -3)
+                        text.insert("-", at: indexToAddHyphen)
+                    }
+                case (true, 12..., false), (false, 14..., false):
+                    text = text.replacingOccurrences(of: "-", with: "")
+                default:
+                    break
+                }
+                textField.text = text
+                return true
+    }
+}
