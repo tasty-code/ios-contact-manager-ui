5b43c02c2a3bfe1b5e3450f4d54b87d4e3ae3349
LeeSe0ngYe0n <goblin__@naver.com>
2024-01-15 17:55:10 +0900

    feat: 유효성 검사 정규식 구현

--- ContactManager/ContactManager/Model/RegularExpressionCheck.swift
+++ ContactManager/ContactManager/Model/RegularExpressionCheck.swift
@@ -2,7 +2,33 @@
 //  IsRegularExpressionCheck.swift
 //  ContactManager
 //
-//  Created by LeeSeongYeon on 2024/01/15.
+//  Created by 유니 & L on 2024/01/15.
 //
 
 import Foundation
+
+enum isRegularExpressionCheck {
+    case name, age, phoneNumber
+    
+    var regex: String {
+        switch self {
+        case .name:
+            return "^[a-zA-Z가-힣 ]+$"
+        case .age:
+            return "^[0-9]{1,3}$"
+        case .phoneNumber:
+            return "^[0-9]{2,3}-[0-9]{3,4}-[0-9]{3,4}$"
+        }
+    }
+    
+    static func isValidString(_ string: String, forPattern pattern: isRegularExpressionCheck) -> Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: pattern.regex)
+            let range = NSRange(location: 0, length: string.utf16.count)
+            return regex.firstMatch(in: string, options: [], range: range) != nil
+        } catch {
+            return false
+        }
+    }
+}
+
