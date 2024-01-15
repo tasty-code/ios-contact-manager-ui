975f30ccb8ff8a48092ea6c46f69146389356ef0
eunjuchoi <eunjuzzoo@naver.com>
2024-01-15 17:19:31 +0900

    refactor: 연락처 형식 검사 메서드 수정 (#3)

--- ContactManager/ContactManager/Sources/Extension/String+.swift
+++ ContactManager/ContactManager/Sources/Extension/String+.swift
@@ -5,22 +5,27 @@
 //  Created by EUNJU on 2024/01/11.
 //
 
-    import Foundation
+import Foundation
 
 extension String {
     
-    func deleteWhiteSpace(_ text: String) -> String {
+    /// 공백 삭제한 문자열 반환하는 메서드
+    func deleteWhiteSpace() -> String {
         return self.filter { $0 != " " }.map { String($0) }.joined()
     }
     
-    func checkValidityAge() -> Bool {
-            let ageCondition = "^[0-9]{1,3}$"
-            return self.range(of: ageCondition, options: .regularExpression) != nil
+    /// 핸드폰 번호 형식인지 검사하는 메서드
+    func validatePhoneNumberFormat() -> Bool {
+        let regex = "^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$"
+        return NSPredicate(format: "SELF MATCHES %@", regex)
+            .evaluate(with: self)
     }
     
-    func checkValidityContact() -> Bool {
-            let contactCondition = "^(0\\d{1,2}-?)?(\\d{9,})$"
-            let checkText = self.replacingOccurrences(of: "[^0-9]", with: "", options: .regularExpression)
-            return checkText.range(of: contactCondition, options: .regularExpression) != nil
+    /// 일반 전화 형식인지 검사하는 메서드
+    func validateContactNumberFormat() -> Bool {
+        let regex = "^0(2|3[1-3]|4[1-4]|5[1-5]|6[1-4])-([0-9]{3,4})-([0-9]{4})$"
+        return NSPredicate(format: "SELF MATCHES %@", regex)
+            .evaluate(with: self)
     }
+    
 }
