0ea5564195ed13267515f45a7d2d9a872dea7b6c
rlwhs12 <150293158+rlwhs12@users.noreply.github.com>
2024-01-15 17:13:18 +0900

    feat: 이름,나이 정규식추가

--- ContactManager/ContactManager/Model/Error.swift
+++ ContactManager/ContactManager/Model/Error.swift
@@ -21,3 +21,20 @@ enum ContactManangerError: Error {
     }
     
 }
+
+enum AddedContactError: Error {
+    case nameMistake
+    case ageMistake
+    case phoneNumberMistake
+    
+    var errorMessage: String {
+        switch self {
+        case .nameMistake:
+            return "입력한 이름 정보가 잘못 되었습니다."
+        case .ageMistake:
+            return "입력한 나이 정보가 잘못 되었습니다."
+        case .phoneNumberMistake:
+            return "입력한 연락처 정보가 잘못 되었습니다."
+        }
+    }
+}
--- ContactManager/ContactManager/ViewController/AddedContactViewController.swift
+++ ContactManager/ContactManager/ViewController/AddedContactViewController.swift
@@ -24,10 +24,7 @@ class AddedContactViewController: UIViewController {
     }
     
     @IBAction func touchUpSaveButton(_ sender: UIButton) {
-        guard let name = nameTextField.text, let age = ageTextField.text, let phoneNumber = phoneNumberTextField.text else { return }
-        guard let age = Int(age) else { return }
-        delegate?.addNewContact(name: name, age: age, phoneNumber: phoneNumber)
-        self.dismiss(animated: true)
+        addDistinguished()
     }
     
     @IBAction func touchUpCancelButton(_ sender: UIButton) {
@@ -35,5 +32,42 @@ class AddedContactViewController: UIViewController {
             self.dismiss(animated: true, completion: nil)
         }
         
+    }
+    
+    func fetchName() throws -> String {
+        let addedName = nameTextField.text
+        guard let name = addedName?.components(separatedBy: [" "]).joined(), String(name).count != 0 else {
+            throw AddedContactError.nameMistake
+        }
+        return name
+    }
+    
+    func fetchAge() throws -> Int {
+        guard let ageText = ageTextField.text,
+                ageText.allSatisfy({ $0.isNumber }),
+                String(ageText).count <= 3,
+                let addedAge = Int(ageText)
+        else {
+            throw AddedContactError.ageMistake
+        }
+        
+        return addedAge
+    }
+    
+    func addDistinguished() {
+        var name: String = String()
+        var age: Int = Int()
+        let isAdded = Result {
+            name = try fetchName()
+            age = try fetchAge()
+        }
+        switch isAdded {
+        case .success():
+            delegate?.addNewContact(name: name, age: age, phoneNumber: name)
+            self.dismiss(animated: true)
+        case .failure(let error):
+            guard let error: AddedContactError = error as? AddedContactError else { return }
+            showAlert(title: "", message: error.errorMessage)
+        }
     }
 }
