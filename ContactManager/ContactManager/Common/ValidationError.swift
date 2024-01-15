dc9bf85aa770b45ff9a43f1e4b86747e8a947fe1
Effie <effie.thedev@gmail.com>
2024-01-15 16:31:14 +0900

    remove: ValidationError 불필요한 구현 제거

--- ContactManager/ContactManager/Common/ValidationError.swift
+++ ContactManager/ContactManager/Common/ValidationError.swift
@@ -21,12 +21,6 @@ enum NameValidationError: ValidationError {
     }
 }
 
-extension NameValidationError: AlertableError {
-    var alertConfig: ErrorAlertConfig {
-        return .init(body: "입력한 이름 정보가 잘못 되었습니다.")
-    }
-}
-
 enum AgeValidationError: ValidationError {
     case shouldBeUnderHundred
     case cannotStartWithZero
@@ -39,12 +33,6 @@ enum AgeValidationError: ValidationError {
     }
 }
 
-extension AgeValidationError: AlertableError {
-    var alertConfig: ErrorAlertConfig {
-        return .init(body: "입력한 나이 정보가 잘못 되었습니다.")
-    }
-}
-
 enum PhoneNumberValidationError: ValidationError {
     case minimumLengthError(_ minimumLength: Int)
     
@@ -53,11 +41,5 @@ enum PhoneNumberValidationError: ValidationError {
         case .minimumLengthError(let length): 
             return "전화번호는 최소 \(length)자리 이상이어야 해요."
         }
-    }
-}
-
-extension PhoneNumberValidationError: AlertableError {
-    var alertConfig: ErrorAlertConfig {
-        return .init(body: "입력한 번호 정보가 잘못 되었습니다.")
     }
 }
