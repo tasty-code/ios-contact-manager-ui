583519ae960261ea44d0c2f98bfa548c2696f691
LeeSe0ngYe0n <goblin__@naver.com>
2024-01-16 14:56:30 +0900

    feat: 유효성 검사 기능 구현 및 은닉화

--- ContactManager/ContactManager/ViewController/NewContactViewController.swift
+++ ContactManager/ContactManager/ViewController/NewContactViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class NewContactViewController: UIViewController, CustomAlert {
-    let contactFileManager: ContactFileManager
+final class NewContactViewController: UIViewController {
+    private let contactFileManager: ContactFileManager
     weak var delegate: UpdateNewContact?
     
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var ageTextField: UITextField!
-    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet private weak var nameTextField: UITextField!
+    @IBOutlet private weak var ageTextField: UITextField!
+    @IBOutlet private weak var phoneNumberTextField: UITextField!
     
     init?(coder: NSCoder, contactFileManager: ContactFileManager) {
         self.contactFileManager = contactFileManager
@@ -30,34 +30,46 @@ class NewContactViewController: UIViewController, CustomAlert {
         phoneNumberTextField.delegate = self
     }
     
-    @IBAction func dismissButtonTapped(_ sender: UIButton) {
-        let cancel = UIAlertAction(title: "아니오", style: .default)
-        let ok = UIAlertAction(title: "예", style: .destructive) { _ in
+    @IBAction private func dismissButtonTapped(_ sender: UIButton) {
+        showAlertWithConfirmation(message: "정말로 취소하시겠습니까?", isDestructive: true) { _ in
             self.dismiss(animated: true)
         }
-        showAlert(message: "정말로 취소하시겠습니까?", actions: [cancel, ok])
-        
     }
     
-    @IBAction func saveButtonTapped(_ sender: UIButton) {
-        guard let name = nameTextField.text,
-               let ageString = ageTextField.text,
-               let phone = phoneNumberTextField.text else { return }
+    @IBAction private func saveButtonTapped(_ sender: UIButton) {
+        guard let name = nameTextField.text?.replacingOccurrences(of: " ", with: ""),
+              let ageString = ageTextField.text,
+              let phone = phoneNumberTextField.text,
+              isInputValidated(name: name, age: ageString, phone: phone) else { return }
+        
         guard let age = Int(ageString) else { return }
         contactFileManager.addContact(contact: Contact(name: name, age: age, phoneNumber: phone))
-        let cancel = UIAlertAction(title: "아니오", style: .default)
-        let ok = UIAlertAction(title: "예", style: .default) { _ in
+        showAlertWithConfirmation(message: "저장하시겠습니까?", isDestructive: false) { _ in
             self.delegate?.updateNewContact()
             self.dismiss(animated: true)
         }
-        showAlert(message: "입력한 정보가 맞는지 확인하세요.", actions: [cancel, ok])
+        
     }
 }
-extension NewContactViewController {
-    func changeKeyboardType() {
+
+extension NewContactViewController{
+    
+    private func changeKeyboardType() {
         nameTextField.keyboardType = .default
         ageTextField.keyboardType = .numberPad
         phoneNumberTextField.keyboardType = .numberPad
+    }
+    
+    private func isInputValidated(name: String, age: String, phone: String) -> Bool {
+        return validateAndShowAlert(input: name, type: .name, error: .nameValidError) && validateAndShowAlert(input: age, type: .age, error: .ageValidError) && validateAndShowAlert(input: phone, type: .phoneNumber, error: .phoneNumberValidError)
+    }
+    
+    private func validateAndShowAlert(input: String, type: RegularExpressionCheck, error: ValidError) -> Bool {
+        guard RegularExpressionCheck.isValidString(string: input, forPattern: type) else {
+            showAlert(message: error.localizedDescription)
+            return false
+        }
+        return true
     }
 }
 
@@ -65,32 +77,29 @@ extension NewContactViewController: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
         guard var text = textField.text else { return false }
+        let prefixValidationCheck = text.hasPrefix("02") || !text.hasPrefix("0")
         
-        switch (text.hasPrefix("02"), text.count, string.isEmpty) {
-                case (true, 2, false), (true, 6, false):
-                    text += "-"
-                case (false, 3, false), (false, 7, false):
-                    text += "-"
-                case (true, 4, true), (true, 8,true):
-                    if string.isEmpty {
-                        text.removeLast()
-                    }
-                case (false, 4,true), (false, 8,true):
-                    if string.isEmpty {
-                        text.removeLast()
-                    }
-                case (true, 11, false), (false, 12, false):
-                    if let lastHyphenIndex = text.lastIndex(of: "-") {
-                        text.remove(at: lastHyphenIndex)
-                        let indexToAddHyphen = text.index(text.endIndex, offsetBy: -3)
-                        text.insert("-", at: indexToAddHyphen)
-                    }
-                case (true, 12..., false), (false, 14..., false):
-                    text = text.replacingOccurrences(of: "-", with: "")
-                default:
-                    break
-                }
-                textField.text = text
-                return true
+        switch (prefixValidationCheck, text.count, string.isEmpty) {
+        case (true, 2, false), (true, 6, false):
+            text += "-"
+        case (false, 3, false), (false, 7, false):
+            text += "-"
+        case (true, 4, true), (true, 8,true):
+            text.removeLast()
+        case (false, 5,true), (false, 9,true):
+            text.removeLast()
+        case (true, 11, false), (false, 12, false):
+            if let lastHyphenIndex = text.lastIndex(of: "-") {
+                text.remove(at: lastHyphenIndex)
+                let indexToAddHyphen = text.index(text.endIndex, offsetBy: -3)
+                text.insert("-", at: indexToAddHyphen)
+            }
+        case (true, 12..., false), (false, 13..., false):
+            text = text.replacingOccurrences(of: "-", with: "")
+        default:
+            break
+        }
+        textField.text = text
+        return true
     }
 }
