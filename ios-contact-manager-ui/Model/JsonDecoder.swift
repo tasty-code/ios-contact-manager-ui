cc974fc3ed1a60a6d38b6a9578b97932226af7d1
hemil0102 <hemil0102@gmail.com>
2024-01-17 15:49:43 +0900

    refactor: 변수명 카멜 케이스 적용 (#2)

--- ios-contact-manager-ui/Model/JsonDecoder.swift
+++ ios-contact-manager-ui/Model/JsonDecoder.swift
@@ -10,19 +10,19 @@ import UIKit
 struct JsonDecoder<Element: Codable> {
     func loadData(from fileName: String, of fileType: String) throws -> Element {
         guard let filePath = Bundle.main.path(forResource: fileName, ofType: fileType) else {
-            print("\(JsonParsingError.pathError.ErrorMessage)")
+            print("\(JsonParsingError.pathError.errorMessage)")
             throw JsonParsingError.pathError }
     
-        guard let content = try? String(contentsOfFile: "filePath") else {
-            print("\(JsonParsingError.stringConversionError.ErrorMessage)")
+        guard let content = try? String(contentsOfFile: filePath) else {
+            print("\(JsonParsingError.stringConversionError.errorMessage)")
             throw JsonParsingError.stringConversionError }
         
         guard let data = content.data(using: .utf8) else { 
-            print("\(JsonParsingError.encodingError.ErrorMessage)")
+            print("\(JsonParsingError.encodingError.errorMessage)")
             throw JsonParsingError.encodingError }
         
         guard let jsonData = try? JSONDecoder().decode(Element.self, from: data) else {
-            print("\(JsonParsingError.decodingError.ErrorMessage)")
+            print("\(JsonParsingError.decodingError.errorMessage)")
             throw JsonParsingError.decodingError }
         
         return jsonData
