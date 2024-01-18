0c495cafcce3f44347bd50f4595a7859469aafad
MyNB1 <dishdib61@naver.com>
2024-01-18 13:50:16 +0900

    refactor: 연락처 유효성 검사로직 리팩토링 (#3)

--- ContactManager/ContactManager/Sources/Scene/NewContactScene/Model/NewContactManager.swift
+++ ContactManager/ContactManager/Sources/Scene/NewContactScene/Model/NewContactManager.swift
@@ -31,20 +31,13 @@ final class NewContactManager {
         digits = digits.replacingOccurrences(of: "-", with: "")
     }
     
-    /// 핸드폰 번호 형식인지 검사하는 메서드
-    private func validatePhoneNumberFormat(_ text: String) -> Bool {
-        let regex = "^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$"
-        return NSPredicate(format: "SELF MATCHES %@", regex)
-            .evaluate(with: text)
-    }
-    
-    /// 일반 전화 형식인지 검사하는 메서드
-    private func validateContactNumberFormat(_ text: String) -> Bool {
-        let regex = "^0(2|3[1-3]|4[1-4]|5[1-5]|6[1-4])-([0-9]{3,4})-([0-9]{4})$"
-        return NSPredicate(format: "SELF MATCHES %@", regex)
+    /// 전화번호 형식 검사 메서드
+    private func validateContactFormat(text: String, format: Contact) -> Bool {
+        return NSPredicate(format: "SELF MATCHES %@", format.regex)
             .evaluate(with: text)
     }
 }
+    
 
 // MARK: - Public Methods
 extension NewContactManager {
@@ -84,8 +77,8 @@ extension NewContactManager {
     
     /// 연락처 정보 유효성 검사 메서드
     func checkValidityOfContactData(nameInput: String?,
-                                            ageInput: String?,
-                                            contactNumberInput: String?) throws -> ContactInfoModel {
+                                    ageInput: String?,
+                                    contactNumberInput: String?) throws -> ContactInfoModel {
         guard let name = nameInput?.deleteWhiteSpace() else {
             throw ContactManagerError.invalidInput(input: .name)
         }
@@ -94,11 +87,11 @@ extension NewContactManager {
             throw ContactManagerError.invalidInput(input: .age)
         }
         guard let contactNumber = contactNumberInput,
-              validatePhoneNumberFormat(contactNumber)
-                || validateContactNumberFormat(contactNumber) else {
+              validateContactFormat(text: contactNumber, format: .normal)
+                || validateContactFormat(text: contactNumber, format: .phone) else {
             throw ContactManagerError.invalidInput(input: .contactNumber)
         }
-
+        
         return ContactInfoModel(name: name, age: age, contactNumber: contactNumber)
     }
 }
