bfe0b885e9e1608413ab83c138da9e85d43d8f16
NERO <mxzxro@gmail.com>
2024-01-20 17:52:32 +0900

    refactor: DetailViewController 리팩토링
    - changeStringsToPerson() 추가 및 관련 메서드 수정
    - MARK 주석 추가

--- ios-contact-manager-ui/ios-contact-manager-ui/Controller/DetailViewController.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Controller/DetailViewController.swift
@@ -33,38 +33,46 @@ private extension DetailViewController {
         let noAction = UIAlertAction(title: "아니오", style: .default)
         
         self.showMessageAlert(message: "정말 취소하시겠습니까?", actionList: [noAction, yesAction])
-        }
+    }
     
     @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
         let name = nameTextField.text?.replacingOccurrences(of: " ", with: "") ?? ""
         let age = ageTextField.text ?? ""
         let phoneNumber = phoneNumberTextField.text ?? ""
+        let dataList = [name, age, phoneNumber]
         
-        let isValidList = isInputTextValid(name: name, age: age, phoneNumber: phoneNumber)
-        isValidCheck(result: isValidList, personData: [name, age, phoneNumber])
+        let isValidList = isInputTextValid(personData: changeStringsToPerson(personStrings: dataList))
+        isValidCheck(boolDict: isValidList, personData: changeStringsToPerson(personStrings: dataList))
+    }
+}
+
+// MARK: - 사용자 입력 RegEx 처리
+private extension DetailViewController {
+    func changeStringsToPerson(personStrings: [String]) -> Person {
+        let result: Person = Person(name: personStrings[0], age: Int(personStrings[1]) ?? 0, phoneNumber: personStrings[2])
+        return result
     }
     
-    func isInputTextValid(name: String, age: String, phoneNumber: String) -> [String: Bool] {
+    func isInputTextValid(personData: Person) -> [String: Bool] {
         let namePattern = "^[a-zA-Z가-힣]*$"
         let agePattern = "^[0-9]{1,3}$"
         let phoneNumberPattern = "^\\d{2,3}-\\d{3,4}-\\d{4}$"
         
-        let isNameValid = NSPredicate(format: "SELF MATCHES %@", namePattern).evaluate(with: name)
-        let isAgeValid = NSPredicate(format: "SELF MATCHES %@", agePattern).evaluate(with: age)
-        let isPhoneNumberValid = NSPredicate(format: "SELF MATCHES %@", phoneNumberPattern).evaluate(with: phoneNumber)
+        let isNameValid = NSPredicate(format: "SELF MATCHES %@", namePattern).evaluate(with: personData.name)
+        let isAgeValid = NSPredicate(format: "SELF MATCHES %@", agePattern).evaluate(with: String(personData.age))
+        let isPhoneNumberValid = NSPredicate(format: "SELF MATCHES %@", phoneNumberPattern).evaluate(with: personData.phoneNumber)
         
         return ["name": isNameValid, "age": isAgeValid, "phoneNumber": isPhoneNumberValid]
     }
     
-    func isValidCheck(result: [String: Bool], personData: [String]) {
+    func isValidCheck(boolDict: [String: Bool], personData: Person) {
         let okAction = UIAlertAction(title: "확인", style: .default)
-        let personData = Person(name: personData[0], age: Int(personData[1]) ?? 0, phoneNumber: personData[2])
         
-        if result["name"] == false {
+        if boolDict["name"] == false {
             self.showMessageAlert(message: "입력한 이름 정보가 잘못되었습니다", actionList: [okAction])
-        } else if result["age"] == false {
+        } else if boolDict["age"] == false {
             self.showMessageAlert(message: "입력한 나이 정보가 잘못되었습니다", actionList: [okAction])
-        } else if result["phoneNumber"] == false {
+        } else if boolDict["phoneNumber"] == false {
             self.showMessageAlert(message: "입력한 연락처 정보가 잘못되었습니다", actionList: [okAction])
         } else {
             model?.addPerson(person: personData)
@@ -74,7 +82,7 @@ private extension DetailViewController {
     }
 }
 
-
+// MARK: - TextField 관련 설정
 extension DetailViewController: UITextFieldDelegate {
     private func keyboardSetup() {
         nameTextField.keyboardType = .default
