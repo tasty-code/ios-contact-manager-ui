97e5f8947675f059883d7fefc60b24d0e62688f6
NERO <mxzxro@gmail.com>
2024-01-20 19:07:06 +0900

    style: 파라미터 정렬

--- ios-contact-manager-ui/ios-contact-manager-ui/Support/UIViewController+.swift
+++ ios-contact-manager-ui/ios-contact-manager-ui/Support/UIViewController+.swift
@@ -3,9 +3,9 @@ import UIKit
 
 extension UIViewController {
     func showMessageAlert(title: String? = nil,
-                                 message: String,
-                                 actionList: [UIAlertAction],
-                                 completion: (() -> ())? = nil) {
+                          message: String,
+                          actionList: [UIAlertAction],
+                          completion: (() -> ())? = nil) {
         let alertViewController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
         for action in actionList {
